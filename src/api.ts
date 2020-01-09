f0ddc89005e0c41276970fe9373efeea6020be4e
Kevin Old <kevin@kevinold.com>
2020-01-08 22:22:40 -0600

    move user routes into module

--- src/api.ts
+++ src/api.ts
@@ -1,17 +1,15 @@
 //require("dotenv").config();
 
-import express, { Request, Response } from "express";
+import express from "express";
 import path from "path";
-import _ from "lodash";
 import logger from "morgan";
 import passport from "passport";
 import session from "express-session";
-import shortid from "shortid";
+
 import bodyParser from "body-parser";
 
-import db from "./backend/database";
 import auth from "./backend/auth";
-import { ensureAuthenticated } from "./backend/helpers";
+import userRoutes from "./backend/user-routes";
 
 const app = express();
 
@@ -26,107 +24,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(auth);
-
-app.get("/users", ensureAuthenticated, (req, res) => {
-  console.log("RU", req.user);
-
-  // TODO: validate order query param(s)
-
-  // TODO:
-  // Query Params:
-  // order
-  //   - default: scoped user contacts first, then all other users
-  //   - "top_first": contacts with most transactions first
-
-  const users = db.get("users").value();
-  res.status(200).json({ users, user: req.user });
-});
-
-app.post("/users", ensureAuthenticated, (req, res) => {
-  // TODO: validate post via joi
-  const user = req.body;
-
-  const id = shortid();
-  user.id = id;
-
-  db.get("users")
-    // @ts-ignore
-    .push(user)
-    .write();
-
-  const record = db
-    .get("users")
-    // @ts-ignore
-    .find({ id })
-    .value();
-
-  res.status(201);
-  res.json({ user: record });
-});
-
-app.get(
-  "/users/:user_id",
-  ensureAuthenticated,
-  (req: Request, res: Response) => {
-    // TODO: validate post via joi
-    const { user_id } = req.params;
-
-    // Permission: account owner
-    // @ts-ignore
-    if (!_.isEqual(user_id, req.user.id)) {
-      return res.status(401).send({
-        error: "Unauthorized"
-      });
-    }
-
-    const user = db
-      .get("users")
-      // @ts-ignore
-      .find({ id: user_id })
-      .value();
-
-    res.status(200);
-    res.json({ user });
-  }
-);
-
-app.get("/users/profile/:username", (req, res) => {
-  // TODO: validate post via joi
-  const { username } = req.params;
-
-  const user = db
-    .get("users")
-    // @ts-ignore
-    .find({ username })
-    .pick(["first_name", "last_name", "avatar"])
-    .value();
-
-  res.status(200);
-  res.json({ user });
-});
-
-app.patch("/users/:user_id", ensureAuthenticated, (req, res) => {
-  // TODO: validate post via joi
-  const { user_id } = req.params;
-
-  const edits = req.body;
-
-  // make update to record
-  db.get("users")
-    // @ts-ignore
-    .find({ id: user_id })
-    .assign(edits)
-    .write();
-
-  const updatedRecord = db
-    .get("users")
-    // @ts-ignore
-    .find({ id: user_id })
-    .value();
-
-  res.status(204);
-  res.json({ user: updatedRecord });
-});
+app.use("/users", userRoutes);
 
 app.use(express.static(path.join(__dirname, "../public")));
 
---
+++ src/backend/user-routes.ts
@@ -0,0 +1,105 @@
+import express, { Request, Response } from "express";
+import _ from "lodash";
+import shortid from "shortid";
+import db from "./database";
+import { ensureAuthenticated } from "./helpers";
+const router = express.Router();
+
+router.get("/", ensureAuthenticated, (req, res) => {
+  console.log("RU", req.user);
+
+  // TODO: validate order query param(s)
+
+  // TODO:
+  // Query Params:
+  // order
+  //   - default: scoped user contacts first, then all other users
+  //   - "top_first": contacts with most transactions first
+
+  const users = db.get("users").value();
+  res.status(200).json({ users, user: req.user });
+});
+
+router.post("/", ensureAuthenticated, (req, res) => {
+  // TODO: validate post via joi
+  const user = req.body;
+
+  const id = shortid();
+  user.id = id;
+
+  db.get("users")
+    // @ts-ignore
+    .push(user)
+    .write();
+
+  const record = db
+    .get("users")
+    // @ts-ignore
+    .find({ id })
+    .value();
+
+  res.status(201);
+  res.json({ user: record });
+});
+
+router.get("/:user_id", ensureAuthenticated, (req: Request, res: Response) => {
+  // TODO: validate post via joi
+  const { user_id } = req.params;
+
+  // Permission: account owner
+  // @ts-ignore
+  if (!_.isEqual(user_id, req.user.id)) {
+    return res.status(401).send({
+      error: "Unauthorized"
+    });
+  }
+
+  const user = db
+    .get("users")
+    // @ts-ignore
+    .find({ id: user_id })
+    .value();
+
+  res.status(200);
+  res.json({ user });
+});
+
+router.get("/profile/:username", (req, res) => {
+  // TODO: validate post via joi
+  const { username } = req.params;
+
+  const user = db
+    .get("users")
+    // @ts-ignore
+    .find({ username })
+    .pick(["first_name", "last_name", "avatar"])
+    .value();
+
+  res.status(200);
+  res.json({ user });
+});
+
+router.patch("/:user_id", ensureAuthenticated, (req, res) => {
+  // TODO: validate post via joi
+  const { user_id } = req.params;
+
+  const edits = req.body;
+
+  // make update to record
+  db.get("users")
+    // @ts-ignore
+    .find({ id: user_id })
+    .assign(edits)
+    .write();
+
+  const updatedRecord = db
+    .get("users")
+    // @ts-ignore
+    .find({ id: user_id })
+    .value();
+
+  res.status(204);
+  res.json({ user: updatedRecord });
+});
+
+export default router;
