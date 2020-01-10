2fe914303d3f0a542ae12b264505ca3d82cb7142
Kevin Old <kevin@kevinold.com>
2020-01-10 16:26:57 -0600

    generate fake bank accounts and update seeds

---
+++ scripts/generateFakeBankAccounts.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+import shortid from "shortid";
+import faker from "faker";
+import { User } from "../src/models/user";
+import { BankAccount } from "../src/models/bankaccount";
+
+const testSeed = require("../src/data/test-seed.json");
+const users = testSeed.users;
+
+const bankAccounts = users.map(
+  (x: User): BankAccount => {
+    return {
+      id: shortid(),
+      uuid: faker.random.uuid(),
+      user_id: x.id,
+      bank_name: `${faker.company.companyName()} Bank`,
+      account_number: faker.finance.account(10),
+      routing_number: faker.finance.account(9),
+      is_deleted: faker.helpers.randomize([true, false]),
+      created_at: faker.date.past(),
+      modified_at: faker.date.recent()
+    };
+  }
+);
+
+fs.writeFile(
+  __dirname + "/bankaccounts.json",
+  JSON.stringify(bankAccounts),
+  function() {
+    console.log("bank account records generated");
+  }
+);
