20cc83cb5032c5d291106e905cef18ead2be6a28
Kevin Old <kevin@kevinold.com>
2020-01-15 10:19:26 -0600

    add like type

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,3 +2,4 @@ export * from "./user";
 export * from "./bankaccount";
 export * from "./contact";
 export * from "./transaction";
+export * from "./like";
---
+++ src/models/like.ts
@@ -0,0 +1,8 @@
+export interface Like {
+  id: string;
+  uuid: string;
+  user_id: string;
+  transaction_id: string;
+  created_at: Date;
+  modified_at: Date;
+}
