d6bd9b5848483e067ed5a99ac603aa61483bbefe
Kevin Old <kevin@kevinold.com>
2020-01-22 16:34:41 -0600

    add simple tests around auth to get test working again

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -7,20 +7,34 @@
 describe("App", function() {
   beforeEach(function() {
     cy.task("db:seed");
-    cy.visit("/");
+  });
+  context("Public Routes", function() {
+    it("renders the signin page", function() {
+      cy.visit("/signin")
+        .get("#root")
+        .should("contain", "Sign In");
+    });
   });
 
-  it("renders the app", function() {
-    cy.getTest("app-name-logo").should("contain", "Pay App");
-  });
+  context("Private Routes", function() {
+    before(function() {
+      cy.fixture("users").as("users");
+      cy.get("@users").then(users => {
+        cy.login(this.users[0].username);
+      });
+    });
+    it("renders the app", function() {
+      cy.getTest("app-name-logo").should("contain", "Pay App");
+    });
 
-  it("defaults side navigation to open", function() {
-    cy.getTest("drawer-icon").should("not.be.visible");
-  });
+    it("defaults side navigation to open", function() {
+      cy.getTest("drawer-icon").should("not.be.visible");
+    });
 
-  it("renders transaction list", function() {
-    cy.getTest("transaction-list")
-      .children()
-      .should("have.length", 1);
+    it("renders transaction list", function() {
+      cy.getTest("transaction-list")
+        .children()
+        .should("have.length", 1);
+    });
   });
 });
