3530170e0c12ccc6d3c3502a9be9763b0ed1cb9a
Kevin Old <kevin@kevinold.com>
2020-01-22 15:46:15 -0600

    configure redux-persist to save state to localstorage

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,9 +1,16 @@
 import axios from "axios";
 import { createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { composeWithDevTools } from "redux-devtools-extension/logOnlyInProduction";
 import { createLogicMiddleware } from "redux-logic";
 import rootReducer from "../reducers";
 import logic from "../logic";
+
+const persistConfig = {
+  key: "root",
+  storage
+};
 
 const logicDeps = {
   httpClient: axios.create({ withCredentials: true })
@@ -20,8 +27,12 @@ if (process.env.NODE_ENV === `development`) {
 
 const enhancer = composeWithDevTools(applyMiddleware(...middlewares));
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export default function configureStore(initialState: object) {
-  const store = createStore(rootReducer, initialState, enhancer);
+  const store = createStore(persistedReducer, initialState, enhancer);
+
+  const persistor = persistStore(store);
 
   if (module.hot) {
     module.hot.accept("../reducers", () =>
@@ -29,5 +40,5 @@ export default function configureStore(initialState: object) {
     );
   }
 
-  return store;
+  return { store, persistor };
 }
