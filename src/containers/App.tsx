14afcd3a89e730d76dd34c65fab1b750d845ed33
Kevin Old <kevin@kevinold.com>
2020-01-23 15:53:29 -0600

    move signin to container and signin component to signinform component

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,13 +3,12 @@ import { Switch, Route } from "react-router";
 import { connect } from "react-redux";
 
 import { bootstrap } from "../actions/app";
-import { signInPending, signOutPending } from "../actions/auth";
+import { signOutPending } from "../actions/auth";
 import { IRootReducerState } from "../reducers";
 import TransactionList from "../components/TransactionList";
 import PrivateRoute from "../components/PrivateRoute";
 import MainLayout from "../components/MainLayout";
-import SignIn from "../components/SignIn";
-import { User } from "../models";
+import SignIn from "../containers/SignIn";
 import SignUp from "../containers/SignUp";
 
 interface StateProps {
@@ -19,7 +18,6 @@ interface StateProps {
 
 interface DispatchProps {
   bootstrapApp: () => void;
-  signInPending: (payload: Partial<User>) => void;
   signOutPending: () => void;
 }
 
@@ -28,7 +26,6 @@ type Props = StateProps & DispatchProps;
 const App: React.FC<Props> = ({
   isBootstrapped,
   bootstrapApp,
-  signInPending,
   signOutPending
 }) => {
   useEffect(() => {
@@ -45,7 +42,7 @@ const App: React.FC<Props> = ({
         </MainLayout>
       </PrivateRoute>
       <Route path="/signin">
-        <SignIn signInPending={signInPending} />
+        <SignIn />
       </Route>
       <Route path="/signup">
         <SignUp />
@@ -61,7 +58,6 @@ const mapStateToProps = (state: IRootReducerState) => ({
 
 const dispatchProps = {
   bootstrapApp: bootstrap,
-  signInPending,
   signOutPending
 };
 
