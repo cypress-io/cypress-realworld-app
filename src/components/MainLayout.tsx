f561ac7a683e064e43cf95f834a598d6f481c5c5
Kevin Old <kevin@kevinold.com>
2020-01-27 10:04:08 -0600

    cleanup

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Box from "@material-ui/core/Box";
