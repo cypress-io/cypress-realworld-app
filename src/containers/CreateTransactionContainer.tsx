f1624543bee75d9a06cc4736c64c643fa95207ba
Kevin Old <kevin@kevinold.com>
2020-01-29 14:43:11 -0600

    setup users reducer and state data

--- src/containers/CreateTransactionContainer.tsx
+++ src/containers/CreateTransactionContainer.tsx
@@ -2,19 +2,25 @@ import React from "react";
 import { connect } from "react-redux";
 import MainContainer from "./MainContainer";
 import { IRootReducerState } from "../reducers";
+import { User } from "../models";
 
-export interface StateProps {}
+export interface StateProps {
+  searchUsers: User[];
+  allUsers: User[];
+}
 
 export type CreateTransactionContainerProps = StateProps;
 
-const CreateTransactionContainer: React.FC<CreateTransactionContainerProps> = ({}) => {
+const CreateTransactionContainer: React.FC<CreateTransactionContainerProps> = ({
+  searchUsers,
+  allUsers
+}) => {
   return <MainContainer>Hello</MainContainer>;
 };
 
 const mapStateToProps = (state: IRootReducerState) => ({
-  //searchUsers: state.users.search,
-  //contacts: state.users.contacts,
-  //allUsers: state.users.all,
+  searchUsers: state.users.search,
+  allUsers: state.users.all
 });
 
 export default connect(mapStateToProps)(CreateTransactionContainer);
