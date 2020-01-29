49e8c68faae199f8e7bad045af0c462d87ea459b
Kevin Old <kevin@kevinold.com>
2020-01-29 12:50:26 -0600

    organize tests

---
+++ cypress/integration/transaction-detail.spec.ts
@@ -0,0 +1,47 @@
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
+
+// check this file using TypeScript if available
+// @ts-check
+
+describe("Transaction Detail", function() {
+  before(function() {
+    cy.fixture("users").as("users");
+    cy.get("@users").then(users => {
+      cy.login(this.users[0].username);
+    });
+    cy.server();
+    cy.route("/transactions/public").as("publicTransactions");
+  });
+  beforeEach(function() {
+    cy.task("db:seed");
+    Cypress.Cookies.preserveOnce("connect.sid");
+  });
+  after(function() {
+    cy.task("db:seed");
+  });
+
+  it("displays transaction detail page", function() {
+    cy.getTest("main").scrollTo("top");
+    cy.wait("@publicTransactions");
+    cy.getTestLike("transaction-item")
+      .first()
+      .scrollIntoView()
+      .click();
+    cy.getTest("nav-transaction-tabs").should("not.be.visible");
+    cy.location("pathname").should("include", "/transaction");
+  });
+
+  it("likes a transaction", function() {
+    cy.getTestLike(`transaction-like-button`).click();
+
+    cy.getTestLike(`transaction-like-count`).should("contain", 1);
+  });
+
+  it("makes a comment on a transaction", function() {
+    cy.getTestLike(`transaction-comment-input`).type(
+      "This is my comment{enter}"
+    );
+    cy.getTestLike(`transaction-comment-count`).should("contain", 4);
+  });
+});
---
+++ cypress/integration/transaction-lists.spec.ts
@@ -0,0 +1,65 @@
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
+
+// check this file using TypeScript if available
+// @ts-check
+
+describe("Transaction Lists", function() {
+  before(function() {
+    cy.fixture("users").as("users");
+    cy.get("@users").then(users => {
+      cy.login(this.users[0].username);
+    });
+  });
+  beforeEach(function() {
+    cy.task("db:seed");
+    Cypress.Cookies.preserveOnce("connect.sid");
+  });
+  after(function() {
+    cy.task("db:seed");
+  });
+
+  it("renders the app", function() {
+    cy.getTest("app-name-logo").should("contain", "Pay App");
+  });
+
+  it("defaults side navigation to open", function() {
+    cy.getTest("drawer-icon").should("not.be.visible");
+  });
+
+  it("renders public transaction lists (contacts, public)", function() {
+    cy.getTest("transaction-list").should("have.length", 2);
+
+    cy.getTest("nav-public-tab").should("have.class", "Mui-selected");
+
+    cy.getTest("transaction-list")
+      .first()
+      .children()
+      .should("have.length", 11);
+
+    cy.getTest("transaction-list")
+      .last()
+      .children()
+      .should("have.length", 5);
+  });
+
+  it("renders contacts transaction list", function() {
+    cy.getTest("main").scrollTo("top");
+    cy.getTest("nav-contacts-tab")
+      .click({ force: true })
+      .should("have.class", "Mui-selected");
+    cy.getTest("transaction-list")
+      .children()
+      .should("have.length", 7);
+  });
+
+  it("renders personal transaction list", function() {
+    cy.getTest("main").scrollTo("top");
+    cy.getTest("nav-personal-tab")
+      .click({ force: true })
+      .should("have.class", "Mui-selected");
+    cy.getTest("transaction-list")
+      .children()
+      .should("have.length", 3);
+  });
+});
