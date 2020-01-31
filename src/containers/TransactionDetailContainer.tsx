a9403d3a510b2c773f0482679e2e1411afb485bc
Kevin Old <kevin@kevinold.com>
2020-01-30 18:04:18 -0600

    cleanup

--- src/containers/TransactionDetailContainer.tsx
+++ src/containers/TransactionDetailContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
 import MainContainer from "./MainContainer";
