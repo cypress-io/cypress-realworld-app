88802be4ec2ac0d7a2388b7b0d7e7c30e228c6fe
Kevin Old <kevin@kevinold.com>
2020-02-05 16:39:10 -0600

    implement and test bank transfer route

---
+++ cypress/integration/api/api-banktransfers.spec.ts
@@ -0,0 +1,36 @@
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
+
+// check this file using TypeScript if available
+// @ts-check
+
+const apiUrl = "http://localhost:3001";
+const apiBankTransfer = `${apiUrl}/bankTransfers`;
+
+describe("Bank Transfer API", function() {
+  before(function() {
+    cy.task("db:seed");
+    cy.fixture("users").as("users");
+    cy.get("@users").then(user => (this.currentUser = this.users[0]));
+  });
+
+  beforeEach(function() {
+    const { username } = this.currentUser;
+    cy.apiLogin(username);
+  });
+
+  afterEach(function() {
+    //cy.task("db:reset");
+    cy.task("db:seed");
+  });
+
+  context("GET /bankTransfer", function() {
+    it("gets a list of bank transfers for user", function() {
+      const { id } = this.currentUser;
+      cy.request("GET", `${apiBankTransfer}`).then(response => {
+        expect(response.status).to.eq(200);
+        expect(response.body.transfers[0].userId).to.eq(id);
+      });
+    });
+  });
+});
