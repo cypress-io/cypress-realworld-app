3763f8ddfbed0c67d83c5e7b0e7f66efdb9c3092
Kevin Old <kevin@kevinold.com>
2020-02-11 13:25:55 -0600

    implement bankaccounts logic

---
+++ src/logic/bankAccountsLogic.ts
@@ -0,0 +1,134 @@
+import { createLogic } from "redux-logic";
+import {
+  BANKACCOUNTS_ALL_PENDING,
+  BANKACCOUNTS_ALL_SUCCESS,
+  BANKACCOUNTS_ALL_ERROR,
+  bankAccountsAllPending,
+  BANKACCOUNT_DETAIL_PENDING,
+  BANKACCOUNT_DETAIL_SUCCESS,
+  BANKACCOUNT_DETAIL_ERROR,
+  bankAccountDetailPending,
+  BANKACCOUNT_CREATE_PENDING,
+  BANKACCOUNT_CREATE_SUCCESS,
+  BANKACCOUNT_CREATE_ERROR,
+  BANKACCOUNT_UPDATE_PENDING,
+  BANKACCOUNT_UPDATE_SUCCESS,
+  BANKACCOUNT_UPDATE_ERROR
+} from "../actions/bankAccounts";
+import { history } from "../index";
+import {
+  isNewBankAccountPath,
+  pathBankAccountId
+} from "../utils/transactionUtils";
+
+const bankAccountsAllLogic = createLogic({
+  type: BANKACCOUNTS_ALL_PENDING,
+  processOptions: {
+    dispatchReturn: true,
+    successType: BANKACCOUNTS_ALL_SUCCESS,
+    failType: BANKACCOUNTS_ALL_ERROR
+  },
+
+  // @ts-ignore
+  process({ httpClient }) {
+    return httpClient
+      .get(`http://localhost:3001/bankAccounts/all`)
+      .then((resp: any) => resp.data.bankAccounts);
+  }
+});
+
+const bankAccountDetailLogic = createLogic({
+  type: BANKACCOUNT_DETAIL_PENDING,
+  processOptions: {
+    dispatchReturn: true,
+    successType: BANKACCOUNT_DETAIL_SUCCESS,
+    failType: BANKACCOUNT_DETAIL_ERROR
+  },
+
+  // @ts-ignore
+  process({ httpClient, action }) {
+    // @ts-ignore
+    const { payload } = action;
+
+    history.push(`/bankAccount/${payload.bankAccountId}`);
+
+    return httpClient
+      .get(`http://localhost:3001/bankAccounts/${payload.bankAccountId}`)
+      .then((resp: any) => resp.data.bankAccount);
+  }
+});
+
+const bankAccountCreateLogic = createLogic({
+  type: BANKACCOUNT_CREATE_PENDING,
+  processOptions: {
+    dispatchReturn: true,
+    successType: BANKACCOUNT_CREATE_SUCCESS,
+    failType: BANKACCOUNT_CREATE_ERROR
+  },
+
+  // @ts-ignore
+  process({ httpClient, action }) {
+    return (
+      httpClient
+        // @ts-ignore
+        .post("http://localhost:3001/bankAccounts", action.payload)
+        .then((resp: any) => resp.data)
+    );
+  }
+});
+
+const bankAccountsRefreshLogic = createLogic({
+  type: [BANKACCOUNT_CREATE_SUCCESS, BANKACCOUNT_UPDATE_SUCCESS],
+
+  // @ts-ignore
+  // eslint-disable-next-line no-empty-pattern
+  process({}, dispatch, done) {
+    const { pathname } = window.location;
+
+    if (isNewBankAccountPath(pathname)) {
+      history.push("/bankaccounts");
+    }
+
+    const bankAccountId = pathBankAccountId(pathname);
+    if (bankAccountId) {
+      dispatch(
+        bankAccountDetailPending({
+          bankAccountId
+        })
+      );
+    }
+
+    dispatch(bankAccountsAllPending());
+    done();
+  }
+});
+
+const bankAccountUpdateLogic = createLogic({
+  type: BANKACCOUNT_UPDATE_PENDING,
+  processOptions: {
+    dispatchReturn: true,
+    successType: BANKACCOUNT_UPDATE_SUCCESS,
+    failType: BANKACCOUNT_UPDATE_ERROR
+  },
+
+  // @ts-ignore
+  process({ httpClient, action }) {
+    // @ts-ignore
+    const { payload } = action;
+
+    return (
+      httpClient
+        // @ts-ignore
+        .patch(`http://localhost:3001/bankAccounts/${payload.id}`, payload)
+        .then((resp: any) => resp.data)
+    );
+  }
+});
+
+export default [
+  bankAccountsAllLogic,
+  bankAccountDetailLogic,
+  bankAccountCreateLogic,
+  bankAccountsRefreshLogic,
+  bankAccountUpdateLogic
+];
