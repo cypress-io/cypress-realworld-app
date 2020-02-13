81cebdb8006f9231909e894468efe2c6c6c03c60
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-02-13 15:42:54 -0500

    describe the rest of the custom commands

--- cypress/global.d.ts
+++ cypress/global.d.ts
@@ -5,6 +5,17 @@
 declare namespace Cypress {
   interface Chainable {
     // describe new "cy" commands here
+
+    /**
+     * Logs in using API request
+     */
     apiLogin(username: string, password?: string): Chainable<Response>
+    /**
+     * Logs in using user interface
+     */
+    login(username: string, password?: string): void
+
+    getTest(dataTestAttribute: string): Chainable<Element>
+    getTestLike(dataTestPrefixAttribute: string): Chainable<Element>
   }
 }
