8a67cde12c2451d0efb1859b71ca2e331b22be78
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-02-13 15:39:51 -0500

    add custom type cy.apiLogin

--- cypress/global.d.ts
+++ cypress/global.d.ts
@@ -2,3 +2,9 @@
 // https://on.cypress.io/typescript
 /// <reference types="cypress" />
 
+declare namespace Cypress {
+  interface Chainable {
+    // describe new "cy" commands here
+    apiLogin(username: string, password?: string): Chainable<Response>
+  }
+}
