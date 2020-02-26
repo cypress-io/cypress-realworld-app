43314f16b6897c642c251334a8c60479457959ae
Kevin Old <kevin@kevinold.com>
2020-02-26 15:03:27 -0600

    add bank accounts empty state

--- src/components/BankAccountList.tsx
+++ src/components/BankAccountList.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import BankAccountItem from "./BankAccountItem";
 import List from "@material-ui/core/List";
 import { BankAccount } from "../models";
+import EmptyList from "./EmptyList";
 
 export interface BankAccountListProps {
   bankAccounts: BankAccount[];
@@ -14,16 +15,21 @@ const BankAccountList: React.FC<BankAccountListProps> = ({
   deleteBankAccount
 }) => {
   return (
-    <List data-test="bankaccount-list">
-      {bankAccounts &&
-        bankAccounts.map((bankAccount: BankAccount) => (
-          <BankAccountItem
-            key={bankAccount.id}
-            bankAccount={bankAccount}
-            deleteBankAccount={deleteBankAccount}
-          />
-        ))}
-    </List>
+    <>
+      {bankAccounts.length > 0 ? (
+        <List data-test="bankaccount-list">
+          {bankAccounts.map((bankAccount: BankAccount) => (
+            <BankAccountItem
+              key={bankAccount.id}
+              bankAccount={bankAccount}
+              deleteBankAccount={deleteBankAccount}
+            />
+          ))}
+        </List>
+      ) : (
+        <EmptyList entity="Bank Accounts" />
+      )}
+    </>
   );
 };
 
