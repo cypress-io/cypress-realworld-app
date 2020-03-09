7dc0fff13cba0ea7ded1c80818159e666f150479
Kevin Old <kevin@kevinold.com>
2020-03-09 09:20:02 -0500

    default to 10 items

--- src/app.ts
+++ src/app.ts
@@ -38,7 +38,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(paginate.middleware(5, 50));
+app.use(paginate.middleware(10, 50));
 
 app.use(auth);
 app.use("/users", userRoutes);
