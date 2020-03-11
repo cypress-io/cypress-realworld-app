7bceabd1375041080ed89fb2f997e9ba512fb943
Kevin Old <kevin@kevinold.com>
2020-03-11 12:46:38 -0500

    comment out skeletonlist

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -5,7 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Link as RouterLink } from "react-router-dom";
 import { Button } from "@material-ui/core";
 
-import SkeletonList from "./SkeletonList";
+//import SkeletonList from "./SkeletonList";
 import { TransactionResponseItem, TransactionPagination } from "../models";
 import EmptyList from "./EmptyList";
 import TransactionInfiniteList from "./TransactionInfiniteList";
