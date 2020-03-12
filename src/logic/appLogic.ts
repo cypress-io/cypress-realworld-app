e1f61f07b95c5f9ecd930d8f4cc8d7bb4161d008
Kevin Old <kevin@kevinold.com>
2020-03-12 15:16:38 -0500

    cleanup

--- src/logic/appLogic.ts
+++ src/logic/appLogic.ts
@@ -1,17 +1,8 @@
 import { createLogic } from "redux-logic";
-import {
-  APP_BOOTSTRAP,
-  APP_SNACKBAR_INIT,
-  APP_SNACKBAR_RESET
-} from "../actions/app";
-import {
-  appBootstrapSuccess,
-  appBootstrapError,
-  appSnackBarReset
-} from "../actions/app";
+import { APP_BOOTSTRAP } from "../actions/app";
+import { appBootstrapSuccess, appBootstrapError } from "../actions/app";
 import { signOutPending } from "../actions/auth";
 import { usersAllPending } from "../actions/users";
-import { notificationsAllPending } from "../actions/notifications";
 import { bankAccountsAllPending } from "../actions/bankaccounts";
 
 const appBootstrapLogic = createLogic({
@@ -47,25 +38,4 @@ const appBootstrapLogic = createLogic({
   }
 });
 
-const appSnackBarResetLogic = createLogic({
-  type: APP_SNACKBAR_RESET,
-  throttle: 4000,
-  validate({ action }, allow) {
-    setTimeout(() => {
-      allow(action);
-    }, 4000);
-  }
-});
-
-const appSnackBarInitLogic = createLogic({
-  type: APP_SNACKBAR_INIT,
-
-  // @ts-ignore
-  // eslint-disable-next-line no-empty-pattern
-  async process({}, dispatch, done) {
-    dispatch(appSnackBarReset());
-    done();
-  }
-});
-
-export default [appBootstrapLogic, appSnackBarResetLogic, appSnackBarInitLogic];
+export default [appBootstrapLogic];
--- src/reducers/app.ts
+++ src/reducers/app.ts
@@ -2,23 +2,16 @@ import {
   TAppReducerActions,
   APP_BOOTSTRAP_PENDING,
   APP_BOOTSTRAP_SUCCESS,
-  APP_BOOTSTRAP_ERROR,
-  APP_SNACKBAR_INIT,
-  APP_SNACKBAR_RESET
+  APP_BOOTSTRAP_ERROR
 } from "../actions/app";
 import { SIGNOUT_SUCCESS, SIGNOUT_ERROR, TAuthActions } from "../actions/auth";
 
 export interface AppState {
   isBootstrapped: boolean;
-  snackbar: object;
 }
 
 const initialState = {
-  isBootstrapped: false,
-  snackbar: {
-    message: "",
-    severity: ""
-  }
+  isBootstrapped: false
 };
 
 export default function reducer(
@@ -35,19 +28,6 @@ export default function reducer(
       return {
         ...state,
         isBootstrapped: true
-      };
-    case APP_SNACKBAR_INIT:
-      return {
-        ...state,
-        snackbar: action.payload
-      };
-    case APP_SNACKBAR_RESET:
-      return {
-        ...state,
-        snackbar: {
-          message: "",
-          severity: ""
-        }
       };
     case APP_BOOTSTRAP_ERROR:
     case SIGNOUT_SUCCESS:
