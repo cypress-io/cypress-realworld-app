6e74a6e469e7d428a43aaab92435fe8b9db4448b
Kevin Old <kevin@kevinold.com>
2020-03-16 17:35:07 -0500

    final cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router } from "react-router-dom";
-import { Provider } from "react-redux";
 import { createBrowserHistory } from "history";
-import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
-import configureStore from "./store/configureStore";
 
 export const history = createBrowserHistory();
 
-const { store, persistor } = configureStore({});
-
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <Router history={history}>
-        <App />
-      </Router>
-    </PersistGate>
-  </Provider>,
+  <Router history={history}>
+    <App />
+  </Router>,
   document.getElementById("root")
 );
 
