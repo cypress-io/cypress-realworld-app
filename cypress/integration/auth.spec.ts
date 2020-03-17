9f770638e2e17ae3cd0a611a69163d50be5d6226
Amir Rustamzadeh <334337+amirrustam@users.noreply.github.com>
2020-03-17 16:27:10 -0400

    refactor user signup and login tests

--- cypress/integration/auth.spec.ts
+++ cypress/integration/auth.spec.ts
@@ -4,41 +4,26 @@
 describe("Authentication", function() {
   beforeEach(function() {
     cy.task("db:seed");
-    Cypress.Cookies.preserveOnce("connect.sid");
-  });
-  after(function() {
-    cy.task("db:seed");
-  });
-  context("User Sign-up", function() {
-    it("signs up a user", function() {
-      cy.visit("/signup")
-        .get("#root")
-        .should("contain", "Sign Up");
-
-      cy.getTest("signup-first-name").type("First");
-      cy.getTest("signup-last-name").type("Last");
-      cy.getTest("signup-username").type("Username");
-
-      cy.getTest("signup-password").type("password");
-      cy.getTest("signup-confirmPassword").type("password");
-
-      cy.getTest("signup-submit").click();
-
-      cy.location("pathname").should("eq", "/signin");
-    });
   });
 
-  context("User Login", function() {
-    before(function() {
-      cy.fixture("users").as("users");
-      cy.get("@users").then(users => {
-        cy.login(this.users[0].username);
-      });
-    });
-    it("logs out", function() {
-      cy.getTest("sidenav-open").click();
-      cy.getTest("sidenav-signout").click();
-      cy.location("pathname").should("eq", "/signin");
-    });
+  it("should allow a visitor to sign-up, login, and logout", function() {
+    // Sign-up User
+    cy.visit("/signup")
+    cy.getTest("signup-first-name").type("First");
+    cy.getTest("signup-last-name").type("Last");
+    cy.getTest("signup-username").type("Username");
+    cy.getTest("signup-password").type("password");
+    cy.getTest("signup-confirmPassword").type("password");
+    cy.getTest("signup-submit").click();
+
+    // Login User
+    cy.getTest("signin-username").type("Username");
+    cy.getTest("signin-password").type("password");
+    cy.getTest("signin-submit").click();
+
+    // // Logout User
+    // cy.getTest("sidenav-open").click();
+    // cy.getTest("sidenav-signout").click();
+    // cy.location("pathname").should("eq", "/signin");
   });
 });
