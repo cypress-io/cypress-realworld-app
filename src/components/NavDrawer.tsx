f426608fb8947b0fe6ee9794fde381f249166b7c
Kevin Old <kevin@kevinold.com>
2020-03-19 08:55:18 -0500

    fix nav link

--- src/components/NavDrawer.tsx
+++ src/components/NavDrawer.tsx
@@ -76,7 +76,7 @@ export const mainListItems = (
       <ListItemIcon>
         <NotificationsIcon />
       </ListItemIcon>
-      <ListItemText primary="Auth" />
+      <ListItemText primary="Notifications" />
     </ListItem>
   </div>
 );
