ad24a7cf0d5e297e890d7b18298cb80046afb138
Kevin Old <kevin@kevinold.com>
2020-04-07 16:59:13 -0500

    add illustration to user settings page

--- src/containers/UserSettingsContainer.tsx
+++ src/containers/UserSettingsContainer.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { makeStyles, Paper, Typography } from "@material-ui/core";
+import { makeStyles, Paper, Typography, Grid } from "@material-ui/core";
 import UserSettingsForm from "../components/UserSettingsForm";
 import { Interpreter } from "xstate";
 import { AuthMachineContext, AuthMachineEvents } from "../machines/authMachine";
 import { useService } from "@xstate/react";
+import { ReactComponent as PersonalSettingsIllustration } from "../svgs/undraw_personal_settings_kihd.svg";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -30,9 +31,25 @@ const UserSettingsContainer: React.FC<Props> = ({ authService }) => {
       <Typography component="h2" variant="h6" color="primary" gutterBottom>
         User Settings
       </Typography>
-      {currentUser && (
-        <UserSettingsForm userProfile={currentUser} updateUser={updateUser} />
-      )}
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justify="flex-start"
+        alignItems="flex-start"
+      >
+        <Grid item>
+          <PersonalSettingsIllustration style={{ height: 200, width: 300 }} />
+        </Grid>
+        <Grid item style={{ width: "50%" }}>
+          {currentUser && (
+            <UserSettingsForm
+              userProfile={currentUser}
+              updateUser={updateUser}
+            />
+          )}
+        </Grid>
+      </Grid>
     </Paper>
   );
 };
