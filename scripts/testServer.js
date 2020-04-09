eb874826e86bf6492b9d2ec7c457c7acbc569167
Kevin Old <kevin@kevinold.com>
2020-04-09 09:18:54 -0500

    rename test server and move to scripts

--- scripts/testServer.js
+++ scripts/testServer.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const path = require("path");
 const history = require("connect-history-api-fallback");
-const setupProxy = require("./src/setupProxy.js");
+const setupProxy = require("../src/setupProxy.js");
 
 const app = express();
 
 setupProxy(app);
 
 app.use(history());
-app.use(express.static(path.join(__dirname, "./build")));
+app.use(express.static(path.join(__dirname, "../build")));
 
 app.listen(3000);
