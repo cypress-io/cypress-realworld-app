228b1a3a8dcc30bd68d7d9dc936941193901b18f
Kevin Old <kevin@kevinold.com>
2020-04-20 13:37:06 -0500

    fix plugin return

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -65,5 +65,5 @@ module.exports = (on, config) => {
   //require('@cypress/code-coverage/task')(on, config)
   // IMPORTANT to return the config object
   // with the any changed environment variables
-  //return config
+  return config;
 };
