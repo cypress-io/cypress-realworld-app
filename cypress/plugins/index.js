a8cb6af6aa8999d5ed51d42b1a98f293f7f50faa
Amir Rustamzadeh <334337+amirrustam@users.noreply.github.com>
2020-04-21 10:02:43 -0400

    add fetch:data task for getting remote seeds

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,21 +1,13 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
 const cypressTypeScriptPreprocessor = require("./cy-ts-preprocessor");
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 const { readFile } = require("fs").promises;
+const { find } = require("lodash/fp");
 const path = require("path");
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
+const axios = require("axios").default;
 
 const getDataFile = (filename) =>
   path.join(__dirname, "../../src/data", filename);
@@ -60,10 +52,15 @@ module.exports = (on, config) => {
       console.log("test database reset");
       return null;
     },
+    "fetch:data"({ entity, findAttrs }) {
+      return axios
+        .get(`http://localhost:3001/testData/${entity}`)
+        .then(({ data }) => find(findAttrs, data.results));
+    },
   });
 
-  require('@cypress/code-coverage/task')(on, config)
+  // require("@cypress/code-coverage/task")(on, config);
   // IMPORTANT to return the config object
   // with the any changed environment variables
-  return config
+  return config;
 };
