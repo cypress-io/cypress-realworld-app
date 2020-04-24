372083e055c24ebe7e05a1755ab67460e399fa02
Amir Rustamzadeh <amir@cypress.io>
2020-04-24 18:32:23 -0400

    fix user settings tests

--- cypress/integration/user-settings.spec.ts
+++ cypress/integration/user-settings.spec.ts
@@ -1,51 +1,32 @@
-// check this file using TypeScript if available
 // @ts-check
+import { User } from "../../src/models";
 
 describe("User Settings", function () {
-  before(function () {
-    cy.fixture("users").as("users");
-    cy.get("@users").then((users) => {
-      cy.login(this.users[0].username);
-    });
-  });
   beforeEach(function () {
     cy.task("db:seed");
-    Cypress.Cookies.preserveOnce("connect.sid");
+
     cy.server();
     cy.route("PATCH", "http://localhost:3001/users/*").as("updateUser");
-    cy.fixture("users").as("users");
+
+    cy.task("find:testData", { entity: "users" }).then((user: User) => {
+      cy.directLogin(user.username);
+    });
 
     cy.getTest("sidenav-user-settings").click();
   });
-  after(function () {
-    cy.task("db:seed");
-  });
 
   it("renders the user settings form", function () {
-    // TODO: Investigate drawer state as solution for flakiness of this test
     cy.getTest("user-settings-form").should("be.visible");
     cy.location("pathname").should("include", "/user/settings");
   });
 
   it("updates first name, last name, email and phone number", function () {
-    cy.getTest("user-settings-firstName-input")
-      .find("input")
-      .clear()
-      .type("New First Name");
-    cy.getTest("user-settings-lastName-input")
-      .find("input")
-      .clear()
-      .type("New Last Name");
-    cy.getTest("user-settings-email-input")
-      .find("input")
-      .clear()
-      .type("email@email.com");
-    cy.getTest("user-settings-phoneNumber-input")
-      .find("input")
-      .clear()
-      .type("6155551212");
-    cy.getTest("user-settings-submit").click();
+    cy.getTestLike("firstName").find("input").clear().type("New First Name");
+    cy.getTestLike("lastName").find("input").clear().type("New Last Name");
+    cy.getTestLike("email").find("input").clear().type("email@email.com");
+    cy.getTestLike("phoneNumber").find("input").clear().type("6155551212");
+    cy.getTestLike("submit").click();
 
-    cy.wait("@updateUser").should("have.property", "status", 204);
+    cy.wait("@updateUser").its("status").should("equal", 204);
   });
 });
