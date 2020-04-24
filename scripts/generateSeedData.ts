116d040f4a8833eff4672fb7bd7c838e7dbff41f
Kevin Old <kevin@kevinold.com>
2020-04-23 20:46:35 -0500

    wip - add recent transaction code and update

--- scripts/generateSeedData.ts
+++ scripts/generateSeedData.ts
@@ -36,6 +36,8 @@ import {
   head,
   mergeWith,
   assignIn,
+  filter,
+  isEqual,
 } from "lodash/fp";
 import {
   BankAccount,
@@ -53,14 +55,13 @@ import {
   TransactionRequestStatus,
   TransactionScenario,
   FakeTransaction,
+  Contact,
 } from "../src/models";
 import {
   getTransactionsForUserContacts,
   getLikesByTransactionId,
   getCommentsByTransactionId,
-  getBankAccountsByUserId,
   TDatabase,
-  createContactForUser,
 } from "../backend/database";
 import { getFakeAmount } from "../src/utils/transactionUtils";
 
@@ -159,16 +160,194 @@ const createSeedBankAccounts = (seedUsers: User[]) =>
     }
   )(seedUsers);
 
+// Transactions
+
+const createTransaction = (
+  type: "payment" | "request",
+  account: BankAccount,
+  details: FakeTransaction
+): Transaction => {
+  const { senderId, receiverId } = details;
+
+  const createdAt = faker.date.past();
+  const modifiedAt = faker.date.recent();
+
+  const status = faker.helpers.randomize([
+    TransactionStatus.pending,
+    TransactionStatus.incomplete,
+    TransactionStatus.complete,
+  ]);
+
+  let requestStatus = "";
+
+  if (type === "request") {
+    requestStatus = TransactionRequestStatus.pending;
+
+    if (status !== TransactionStatus.incomplete) {
+      requestStatus = faker.helpers.randomize([
+        TransactionRequestStatus.pending,
+        TransactionRequestStatus.accepted,
+        TransactionRequestStatus.rejected,
+      ]);
+    }
+
+    if (status === TransactionStatus.complete) {
+      requestStatus = faker.helpers.randomize([
+        TransactionRequestStatus.accepted,
+        TransactionRequestStatus.rejected,
+      ]);
+    }
+  }
+
+  const requestResolvedAt =
+    requestStatus === TransactionRequestStatus.pending
+      ? ""
+      : faker.date.future(undefined, createdAt);
+
+  return {
+    id: shortid(),
+    uuid: faker.random.uuid(),
+    source: account.id,
+    amount: getFakeAmount(),
+    description: isPayment(type)
+      ? `Payment: ${senderId} to ${receiverId}`
+      : `Request: ${receiverId} to ${senderId}`,
+    privacyLevel: faker.helpers.randomize([
+      DefaultPrivacyLevel.public,
+      DefaultPrivacyLevel.private,
+      DefaultPrivacyLevel.contacts,
+    ]),
+    receiverId,
+    senderId,
+    balanceAtCompletion: getFakeAmount(),
+    status,
+    requestStatus,
+    requestResolvedAt,
+    createdAt,
+    modifiedAt,
+  };
+};
+
+const createPayment = (account: BankAccount, user: User, randomUser: User) => {
+  const paymentScenarios: TransactionScenario[] = [
+    {
+      status: TransactionStatus.pending,
+      requestStatus: "",
+    },
+    {
+      status: TransactionStatus.incomplete,
+      requestStatus: "",
+    },
+    {
+      status: TransactionStatus.complete,
+      requestStatus: "",
+    },
+  ];
+
+  const allScenarios = paymentScenarios.map((details) => {
+    const paymentTransaction = createTransaction("payment", account, {
+      senderId: user.id,
+      receiverId: randomUser.id,
+      ...details,
+    });
+
+    const paymentInverseTransaction = createTransaction("payment", account, {
+      senderId: randomUser.id,
+      receiverId: user.id,
+      ...details,
+    });
+
+    return [paymentTransaction, paymentInverseTransaction];
+  });
+
+  return flattenDeep(allScenarios);
+};
+
+const createRequest = (account: BankAccount, user: User, randomUser: User) => {
+  const requestScenarios: TransactionScenario[] = [
+    {
+      status: TransactionStatus.pending,
+      requestStatus: "pending",
+    },
+    {
+      status: TransactionStatus.incomplete,
+      requestStatus: "pending",
+    },
+    {
+      status: TransactionStatus.complete,
+      requestStatus: "accepted",
+    },
+    {
+      status: TransactionStatus.complete,
+      requestStatus: "rejected",
+    },
+  ];
+
+  const allScenarios = requestScenarios.map((details) => {
+    const requestTransaction = createTransaction("request", account, {
+      senderId: user.id,
+      receiverId: randomUser.id,
+      ...details,
+    });
+
+    const requestInverseTransaction = createTransaction("request", account, {
+      senderId: randomUser.id,
+      receiverId: user.id,
+      ...details,
+    });
+
+    return [requestTransaction, requestInverseTransaction];
+  });
+
+  return flattenDeep(allScenarios);
+};
+
+const getBankAccountsByUserId = (
+  seedBankAccounts: BankAccount[],
+  userId: User["id"]
+): BankAccount[] =>
+  filter(flow(get("userId"), isEqual(userId)), seedBankAccounts);
+
+const createSeedTransactions = (
+  seedUsers: User[],
+  seedBankAccounts: BankAccount[]
+) =>
+  flattenDeep(
+    map((user: User): Transaction[][] => {
+      const accounts = getBankAccountsByUserId(seedBankAccounts, user.id);
+      const randomUser = getOtherRandomUser(seedUsers, user.id);
+
+      return accounts.map((account: BankAccount) => {
+        // @ts-ignore
+        const payments = times(
+          () => createPayment(account, user, randomUser),
+          paymentsPerUser
+        );
+        // @ts-ignore
+        const requests = times(
+          () => createRequest(account, user, randomUser),
+          requestsPerUser
+        );
+
+        return concat(payments, requests);
+      });
+    })(seedUsers)
+  );
+
 export const buildDatabase = () => {
-  const seedUsers = createSeedUsers();
-  const seedContacts = createSeedContacts(seedUsers);
-  const seedBankAccounts = createSeedBankAccounts(seedUsers);
+  const seedUsers: User[] = createSeedUsers();
+  const seedContacts: Contact[] = createSeedContacts(seedUsers);
+  const seedBankAccounts: BankAccount[] = createSeedBankAccounts(seedUsers);
+  const seedTransactions: Transaction[] = createSeedTransactions(
+    seedUsers,
+    seedBankAccounts
+  );
 
   return {
     users: seedUsers,
     contacts: seedContacts,
     bankaccounts: seedBankAccounts,
-    transactions: [],
+    transactions: seedTransactions,
     likes: [],
     comments: [],
     notifications: [],
@@ -177,9 +356,6 @@ export const buildDatabase = () => {
 };
 
 /*
-// TRANSACTIONS
-
-const seedTransactions: Transaction[] = flattenDeep(transactions);
 
 const createFakeLike = (userId: string, transactionId: string): Like => ({
   id: shortid(),
