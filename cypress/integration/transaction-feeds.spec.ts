20648694b5b41fb7fc1cd98ad92ec727cc7cd0d2
Amir Rustamzadeh <amir@cypress.io>
2020-04-27 18:23:02 -0400

    refactor transaction feeds tests

--- cypress/integration/transaction-feeds.spec.ts
+++ cypress/integration/transaction-feeds.spec.ts
@@ -1,6 +1,7 @@
 // @ts-check
 
-import { User } from "../../src/models";
+import { User, Transaction } from "../../src/models";
+import { format as formatDate, addDays, isWithinRange } from "date-fns";
 
 type TransactionFeedsCtx = {
   allUsers?: User[];
@@ -39,187 +40,142 @@ describe("Transaction Feed", function () {
     cy.getTest("sidenav-user-balance").should("be.visible");
   });
 
-  it.skip("renders everyone (public) (infinite list)", function () {
-    // Discussion: Should repeated assertions like this be wrapped up in commands
-    cy.getTest("nav-public-tab")
-      .should("have.class", "Mui-selected")
-      .contains("everyone", { matchCase: false })
-      .should("have.css", { "text-transform": "uppercase" });
+  const feedViews = [
+    {
+      tab: "nav-public-tab",
+      tabLabel: "everyone",
+      routeAlias: "publicTransactions",
+      service: "publicTransactionService",
+    },
+    {
+      tab: "nav-contacts-tab",
+      tabLabel: "friends",
+      routeAlias: "contactsTransactions",
+      service: "contactTransactionService",
+    },
+    {
+      tab: "nav-personal-tab",
+      tabLabel: "mine",
+      routeAlias: "personalTransactions",
+      service: "personalTransactionService",
+    },
+  ];
 
-    // Assert at the network layer
-    // TODO: make 10 an importable constant
-    cy.wait("@publicTransactions")
-      .its("response.body.results")
-      .should("have.length", 10);
+  feedViews.forEach((feed) => {
+    it(`renders ${feed.tabLabel} transaction feed (infinite list)`, function () {
+      cy.getTest(feed.tab)
+        .click()
+        .should("have.class", "Mui-selected")
+        .contains(feed.tabLabel, { matchCase: false })
+        .should("have.css", { "text-transform": "uppercase" });
 
-    cy.getTestLike("transaction-item").should("have.length", 8);
+      // Assert at the network layer
+      cy.wait(`@${feed.routeAlias}`)
+        .its("response.body.results")
+        .should("have.length", Cypress.env("paginationPageSize"));
 
-    cy.getTest("transaction-list").children().scrollTo("bottom");
+      // Assert visible UI
+      cy.getTestLike("transaction-item").should("have.length", 8);
 
-    cy.wait("@publicTransactions")
-      .its("response.body")
-      .then(({ results, pageData }) => {
-        expect(results).have.length(10);
-        expect(pageData.page).to.equal(2);
-      });
+      // Scroll to paginate to next page
+      cy.getTest("transaction-list").children().scrollTo("bottom");
 
-    // TODO: asserting network call count
-    // cy.wait("@publicTransactions").should("have.callCount", 2);
-    // cy.wait("@publicTransactions").should("be.calledTwice");
-  });
+      // TODO: Flakey assertion, seems to only fail for personal tab
+      cy.getTest("transaction-loading").should("be.visible");
 
-  it.skip("renders everyone (public) (infinite list)", function () {
-    // Options for Testing Paginated views
-    // - Assert at the network layer
-    // - Assert at the UI layer
-    // - Assert internal state
-
-    // Discussion: Should repeated assertions like this be wrapped up in commands
-    cy.getTest("nav-public-tab")
-      .should("have.class", "Mui-selected")
-      .contains("everyone", { matchCase: false })
-      .should("have.css", { "text-transform": "uppercase" });
-
-    // Assert at the network layer
-    cy.wait("@publicTransactions")
-      .its("response.body")
-      .then(({ results, pageData }) => {
-        // TODO: use satisfy assertion
-        if (pageData.totalPages > 1) {
-          expect(results).to.have.lengthOf(10);
-        } else {
-          expect(results).to.have.at.least(10);
-        }
-
-        expect(pageData.page).to.equal(1);
-
-        Array.from({ length: pageData.totalPages }, (v, pageIndex) => {
-          const currentPage = pageIndex + 2;
-
-          cy.getTest("transaction-list").children().scrollTo("bottom");
-          cy.getTest("transaction-loading").should("be.visible");
-
-          if (currentPage < pageData.totalPages) {
-            cy.wait("@publicTransactions")
-              .its("response.body.results")
-              .then(({ results, pageData }) => {
-                expect(pageData.page).to.equal(currentPage);
-
-                if (pageData.page < pageData.totalPages) {
-                  expect(results).to.have.lengthOf(10); // this runs first because it's sync
-                  return cy
-                    .getTest("transaction-loading")
-                    .should("not.be.visible");
-                }
-
-                expect(results.length).to.be.at.least(1);
-              });
-          }
+      cy.wait(`@${feed.routeAlias}`)
+        .its("response.body")
+        .then(({ results, pageData }) => {
+          expect(results).have.length(Cypress.env("paginationPageSize"));
+          expect(pageData.page).to.equal(2);
+          // @ts-ignore
+          cy.nextTransactionFeedPage(feed.service, pageData.totalPages);
         });
-      });
 
-    cy.getTestLike("transaction-item").should("have.length", 10);
-  });
-
-  it("renders friends (contacts) transaction feed  (infinite list)", function () {
-    cy.getTest("nav-contacts-tab") // On get Navigation tabs are hidden under the AppBar in the UI
-      .scrollIntoView() // TODO: Bug? Does not work as expected to scroll the tab into view
-      .click({ force: true }); // Current solution is to force the click
-    cy.getTestLike("transaction-item").should("have.length", 8);
-    cy.getTest("nav-contacts-tab").should("have.class", "Mui-selected");
-    cy.getTest("transaction-list").children().scrollTo("bottom");
-    cy.getTest("transaction-loading").should("be.visible");
-    cy.wait("@contactsTransactions");
-    cy.getTest("transaction-loading").should("not.be.visible");
-    // DISCUSS: the most effective assertion for infinite scroll
-    cy.getTestLike("transaction-item").should("have.length.greaterThan", 5);
-  });
-
-  it("renders mine (personal) transaction feed (infinite list)", function () {
-    cy.getTest("nav-personal-tab").click({ force: true });
-    cy.getTest("nav-personal-tab").should("have.class", "Mui-selected");
-
-    cy.getTestLike("transaction-item").should("have.length", 8);
-
-    // TODO: Test infinite scrolling by adding more personal transactions
-    /*
-    cy.getTest("transaction-list")
-      .children()
-      .scrollTo("bottom");
-
-    cy.getTest("transaction-loading").should("be.visible");
-    cy.wait("@personalTransactions");
-    cy.getTest("transaction-loading").should("not.be.visible");
-
-    // DISCUSS: the most effective assertion for infinite scroll
-    cy.getTestLike("transaction-item").should("have.length.greaterThan", 5);
-    */
+      cy.wait(`@${feed.routeAlias}`)
+        .its("response.body")
+        .then(({ results, pageData }) => {
+          expect(results).to.have.length.greaterThan(1);
+          expect(pageData.page).to.equal(pageData.totalPages);
+          expect(pageData.hasNextPages).to.equal(false);
+        });
+    });
   });
 
   it("shows date range calendar full screen on mobile", function () {
     cy.viewport("iphone-6");
-    cy.getTest("main").scrollTo("top");
-    cy.getTest("nav-personal-tab")
-      .click({ force: true })
-      .should("have.class", "Mui-selected");
 
-    cy.getTest("transaction-list-filter-date-range-button")
+    cy.getTest("nav-personal-tab").click().should("have.class", "Mui-selected");
+
+    cy.getTestLike("filter-date-range-button")
       .scrollIntoView()
       .click({ force: true });
 
     cy.getTest("date-range-filter-drawer").should("be.visible");
-    // DISCUSS:
-    // Testing for hidden elements fail
-    // and appear to be related to issues filed against Cypress
+
+    // Potential Cypress Bug:
+    // This is a potential bug with two overlapping fixed elements
     // https://github.com/cypress-io/cypress/issues/1242
     // https://github.com/cypress-io/cypress/issues/5959
-    //cy.getTest("app-name-logo").should("be.hidden");
-    //cy.getTest("app-name-logo").should("not.be.visible");
+    // cy.getTest("app-name-logo").should("not.be.visible");
 
     cy.getTest("date-range-filter-drawer-close").click();
   });
 
   it("shows amount range in drawer on mobile", function () {
     cy.viewport("iphone-6");
-    cy.getTest("main").scrollTo("top");
-    cy.getTest("nav-personal-tab")
-      .click({ force: true })
-      .should("have.class", "Mui-selected");
 
-    cy.getTest("transaction-list-filter-amount-range-button")
-      .scrollIntoView()
-      .click({ force: true });
-
+    cy.getTest("nav-personal-tab").click().should("have.class", "Mui-selected");
+    cy.getTestLike("filter-amount-range-button").click({ force: true });
     cy.getTest("amount-range-filter-drawer").should("be.visible");
-
     cy.getTest("amount-range-filter-drawer-close").click();
   });
 
   it("renders mine (personal) transaction feed, filters by date range, then clears the date range filter", function () {
-    cy.wrap(new Date(2019, 11, 1).getTime()).then((now) => {
-      cy.clock(now, ["Date"]);
+    cy.task("find:testData", {
+      entity: "transactions",
+    }).then((transaction: Transaction) => {
+      const dateFormat = "YYYY-MM-DD";
+      const dateRangeStart = new Date(transaction.createdAt);
+      const dateRangeEnd = addDays(dateRangeStart, 1);
+
+      cy.wrap(dateRangeStart.getTime()).then((now) => {
+        cy.clock(now, ["Date"]);
+      });
 
       cy.getTest("nav-personal-tab")
         .click()
         .should("have.class", "Mui-selected");
 
-      cy.getTest("main").scrollTo("top");
-      cy.getTest("transaction-list-filter-date-range-button").click({
+      cy.wait("@personalTransactions");
+
+      cy.getTestLike("filter-date-range-button").click({ force: true });
+
+      cy.get(`[data-date='${formatDate(dateRangeStart, dateFormat)}']`).click({
+        force: true,
+      });
+      cy.get(`[data-date='${formatDate(dateRangeEnd, dateFormat)}']`).click({
         force: true,
       });
 
-      cy.get("[data-date='2019-12-01']").click({ force: true });
-      cy.get("[data-date='2019-12-03']").click({ force: true });
+      cy.wait("@personalTransactions")
+        .its("response.body.results")
+        .then((transactions: Transaction[]) => {
+          cy.getTestLike("transaction-item").should(
+            "have.length",
+            transactions.length
+          );
 
-      cy.wait("@personalTransactions");
+          transactions.forEach(({ createdAt }) => {
+            expect(
+              isWithinRange(createdAt, dateRangeStart, dateRangeEnd)
+            ).to.equal(true);
+          });
+        });
 
-      cy.getTestLike("transaction-item").should("have.length", 3);
-
-      cy.getTest("transaction-list-filter-date-clear-button")
-        .scrollIntoView()
-        .click({ force: true });
-
-      cy.getTestLike("transaction-item").should("have.length.greaterThan", 3);
+      // cy.getTest("transaction-list-filter-date-clear-button").click({
+      //   force: true,
+      // });
     });
   });
 
@@ -249,12 +205,13 @@ describe("Transaction Feed", function () {
       .scrollIntoView()
       .click({ force: true });
 
+    cy.window().invoke("handleAmountRangeChange", [null, [2, 50]]);
+
     // DISCUSS:
     // How to set hidden input values like this?
-    cy.getTest("transaction-list-filter-amount-range-slider")
-      .find('input[type="hidden"]')
-      .invoke("val", "3,10")
-      .trigger("input", { force: true });
+    // cy.getTest("transaction-list-filter-amount-range-slider")
+    //   .find("[data-index='0']")
+    //   .trigger("mousedown");
 
     cy.getTest("transaction-list-filter-amount-range-text")
       .should("contain", "$30")
