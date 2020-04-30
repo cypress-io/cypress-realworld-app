2be30b2d4be8ee28f091f888226032f0f50e55ba
Amir Rustamzadeh <amir@cypress.io>
2020-04-30 12:40:36 -0400

    improve dynamic generation of tests for transaction feed

--- cypress/tests/ui/transaction-feeds.spec.ts
+++ cypress/tests/ui/transaction-feeds.spec.ts
@@ -5,6 +5,8 @@ import {
   endOfDayUTC,
 } from "../../../src/utils/transactionUtils";
 
+const { _ } = Cypress;
+
 type TransactionFeedsCtx = {
   allUsers?: User[];
   user?: User;
@@ -13,13 +15,34 @@ type TransactionFeedsCtx = {
 describe("Transaction Feed", function () {
   const ctx: TransactionFeedsCtx = {};
 
+  const feedViews = {
+    public: {
+      tab: "public-tab",
+      tabLabel: "everyone",
+      routeAlias: "publicTransactions",
+      service: "publicTransactionService",
+    },
+    contacts: {
+      tab: "contacts-tab",
+      tabLabel: "friends",
+      routeAlias: "contactsTransactions",
+      service: "contactTransactionService",
+    },
+    personal: {
+      tab: "personal-tab",
+      tabLabel: "mine",
+      routeAlias: "personalTransactions",
+      service: "personalTransactionService",
+    },
+  };
+
   beforeEach(function () {
     cy.task("db:seed");
 
     cy.server();
-    cy.route("/transactions*").as("personalTransactions");
-    cy.route("/transactions/public*").as("publicTransactions");
-    cy.route("/transactions/contacts*").as("contactsTransactions");
+    cy.route("/transactions*").as(feedViews.personal.routeAlias);
+    cy.route("/transactions/public*").as(feedViews.public.routeAlias);
+    cy.route("/transactions/contacts*").as(feedViews.contacts.routeAlias);
 
     cy.task("filter:testData", { entity: "users" }).then((users: User[]) => {
       ctx.user = users[0];
@@ -75,33 +98,9 @@ describe("Transaction Feed", function () {
   });
 
   describe("renders and paginates all transaction feeds", function () {
-    const feedViews = [
-      // TODO: the new seed for public transactions currently only has 5 transactions,
-      //       which is less than the pagination page limit. This introduces a new test case
-      //       where a paginated view renders a list with only 1 page to load
-      {
-        tab: "nav-public-tab",
-        tabLabel: "everyone",
-        routeAlias: "publicTransactions",
-        service: "publicTransactionService",
-      },
-      {
-        tab: "nav-contacts-tab",
-        tabLabel: "friends",
-        routeAlias: "contactsTransactions",
-        service: "contactTransactionService",
-      },
-      {
-        tab: "nav-personal-tab",
-        tabLabel: "mine",
-        routeAlias: "personalTransactions",
-        service: "personalTransactionService",
-      },
-    ];
-
-    feedViews.forEach((feed) => {
-      it(`renders and paginates ${feed.tabLabel} transaction feed`, function () {
-        cy.getTest(feed.tab)
+    _.each(feedViews, (feed, feedName) => {
+      it(`renders and paginates ${feedName} transaction feed`, function () {
+        cy.getTestLike(feed.tab)
           .click()
           .should("have.class", "Mui-selected")
           .contains(feed.tabLabel, { matchCase: false })
@@ -142,56 +141,56 @@ describe("Transaction Feed", function () {
   });
 
   describe("filters transaction feeds by date range", function () {
-    it("filters mine (personal) transaction feed by date range", function () {
-      cy.task("find:testData", {
-        entity: "transactions",
-      }).then((transaction: Transaction) => {
-        const dateRangeStart = startOfDay(new Date(transaction.createdAt));
-        const dateRangeEnd = endOfDayUTC(addDays(dateRangeStart, 1));
+    _.each(feedViews, (feed, feedName) => {
+      it(`filters ${feedName} transaction feed by date range`, function () {
+        cy.task("find:testData", {
+          entity: "transactions",
+        }).then((transaction: Transaction) => {
+          const dateRangeStart = startOfDay(new Date(transaction.createdAt));
+          const dateRangeEnd = endOfDayUTC(addDays(dateRangeStart, 1));
 
-        cy.getTest("nav-personal-tab")
-          .click()
-          .should("have.class", "Mui-selected");
+          cy.getTestLike(feed.tab).click().should("have.class", "Mui-selected");
 
-        cy.wait("@personalTransactions")
-          .its("response.body.results")
-          .as("unfilteredResults");
+          cy.wait(`@${feed.routeAlias}`)
+            .its("response.body.results")
+            .as("unfilteredResults");
 
-        cy.pickDateRange(dateRangeStart, dateRangeEnd);
+          cy.pickDateRange(dateRangeStart, dateRangeEnd);
 
-        cy.wait("@personalTransactions")
-          .its("response.body.results")
-          .then((transactions: Transaction[]) => {
-            cy.getTestLike("transaction-item").should(
-              "have.length",
-              transactions.length
-            );
+          cy.wait(`@${feed.routeAlias}`)
+            .its("response.body.results")
+            .then((transactions: Transaction[]) => {
+              cy.getTestLike("transaction-item").should(
+                "have.length",
+                transactions.length
+              );
 
-            transactions.forEach(({ createdAt }) => {
-              const createdAtDate = startOfDayUTC(new Date(createdAt));
+              transactions.forEach(({ createdAt }) => {
+                const createdAtDate = startOfDayUTC(new Date(createdAt));
 
-              expect(
-                isWithinInterval(createdAtDate, {
-                  start: startOfDayUTC(dateRangeStart),
-                  end: dateRangeEnd,
-                }),
-                `transaction created date (${createdAtDate.toISOString()}) 
-                is within ${dateRangeStart.toISOString()} 
-                and ${dateRangeEnd.toISOString()}`
-              ).to.equal(true);
+                expect(
+                  isWithinInterval(createdAtDate, {
+                    start: startOfDayUTC(dateRangeStart),
+                    end: dateRangeEnd,
+                  }),
+                  `transaction created date (${createdAtDate.toISOString()}) 
+                  is within ${dateRangeStart.toISOString()} 
+                  and ${dateRangeEnd.toISOString()}`
+                ).to.equal(true);
+              });
             });
-          });
 
-        cy.getTestLike("filter-date-clear-button").click({ force: true });
+          cy.getTestLike("filter-date-clear-button").click({ force: true });
 
-        cy.get("@unfilteredResults").then(
-          // @ts-ignore
-          (unfilteredResults: Transaction[]) => {
-            cy.wait("@personalTransactions")
-              .its("response.body.results")
-              .should("deep.equal", unfilteredResults);
-          }
-        );
+          cy.get("@unfilteredResults").then(
+            // @ts-ignore
+            (unfilteredResults: Transaction[]) => {
+              cy.wait(`@${feed.routeAlias}`)
+                .its("response.body.results")
+                .should("deep.equal", unfilteredResults);
+            }
+          );
+        });
       });
     });
   });
@@ -199,30 +198,32 @@ describe("Transaction Feed", function () {
   // TODO: add a test to filter for transaction out of known seed date range limit
 
   describe("filters transaction feeds by amount range", function () {
-    it("filters public (everyone) transaction feed by amount range", function () {
-      cy.getTest("nav-public-tab")
-        .click({ force: true })
-        .should("have.class", "Mui-selected");
+    _.each(feedViews, (feed, feedName) => {
+      it(`filters ${feedName} transaction feed by amount range`, function () {
+        cy.getTestLike(feed.tab)
+          .click({ force: true })
+          .should("have.class", "Mui-selected");
 
-      cy.getTest("transaction-list-filter-amount-range-button")
-        .scrollIntoView()
-        .click({ force: true });
+        cy.getTest("transaction-list-filter-amount-range-button")
+          .scrollIntoView()
+          .click({ force: true });
 
-      cy.get("[data-index='0']")
-        .trigger("mousedown", { force: true })
-        .trigger("mousemove", 10, -10, { force: true });
+        cy.get("[data-index='0']")
+          .trigger("mousedown", { force: true })
+          .trigger("mousemove", 10, -10, { force: true });
 
-      cy.get("[data-index='1']")
-        .trigger("mousedown", { force: true })
-        .trigger("mousemove", -100, 100, { force: true });
+        cy.get("[data-index='1']")
+          .trigger("mousedown", { force: true })
+          .trigger("mousemove", -100, 100, { force: true });
 
-      cy.getTest("transaction-list-filter-amount-range-text")
-        .should("contain", "$40")
-        .and("contain", "$300");
+        cy.getTest("transaction-list-filter-amount-range-text")
+          .should("contain", "$40")
+          .and("contain", "$300");
 
-      cy.wait("@publicTransactions");
+        cy.wait(`@${feed.routeAlias}`);
 
-      cy.getTestLike("transaction-item").should("have.length.greaterThan", 1);
+        cy.getTestLike("transaction-item").should("have.length.greaterThan", 1);
+      });
     });
   });
 });
