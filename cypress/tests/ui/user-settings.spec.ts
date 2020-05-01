e5aaaf84e4d5497614fd6d945aafdbe71901b964
Kevin Old <kevin@kevinold.com>
2020-05-01 16:23:15 -0500

    implement and test user settings form errors

--- cypress/tests/ui/user-settings.spec.ts
+++ cypress/tests/ui/user-settings.spec.ts
@@ -20,11 +20,56 @@ describe("User Settings", function () {
     cy.location("pathname").should("include", "/user/settings");
   });
 
+  it("should display user setting form errors", function () {
+    ["first", "last"].forEach((field) => {
+      cy.getTestLike(`${field}Name-input`)
+        .type("Abc")
+        .find("input")
+        .clear()
+        .blur();
+      cy.get(`#user-settings-${field}Name-input-helper-text`)
+        .should("be.visible")
+        .and("contain", `Enter a ${field} name`);
+    });
+
+    cy.getTestLike("email-input").type("abc").find("input").clear().blur();
+    cy.get("#user-settings-email-input-helper-text")
+      .should("be.visible")
+      .and("contain", "Enter an email address");
+
+    cy.getTestLike("email-input").type("abc@bob.").find("input").blur();
+    cy.get("#user-settings-email-input-helper-text")
+      .should("be.visible")
+      .and("contain", "Must contain a valid email address");
+
+    cy.getTestLike("phoneNumber-input")
+      .type("abc")
+      .find("input")
+      .clear()
+      .blur();
+    cy.get("#user-settings-phoneNumber-input-helper-text")
+      .should("be.visible")
+      .and("contain", "Enter a phone number");
+
+    cy.getTestLike("phoneNumber-input").type("615-555-").find("input").blur();
+    cy.get("#user-settings-phoneNumber-input-helper-text")
+      .should("be.visible")
+      .and("contain", "Phone number is not valid");
+
+    cy.getTestLike("submit").should("be.disabled");
+  });
+
   it("updates first name, last name, email and phone number", function () {
     cy.getTestLike("firstName").find("input").clear().type("New First Name");
     cy.getTestLike("lastName").find("input").clear().type("New Last Name");
     cy.getTestLike("email").find("input").clear().type("email@email.com");
-    cy.getTestLike("phoneNumber").find("input").clear().type("6155551212");
+    cy.getTestLike("phoneNumber-input")
+      .find("input")
+      .clear()
+      .type("6155551212")
+      .blur();
+
+    cy.getTestLike("submit").should("not.be.disabled");
     cy.getTestLike("submit").click();
 
     cy.wait("@updateUser").its("status").should("equal", 204);
