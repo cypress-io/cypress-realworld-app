419486da8462fe56041f606f6c94bc994574a06f
Kevin Old <kevin@kevinold.com>
2020-05-06 11:10:45 -0500

    enable code coverage again

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -35,7 +35,7 @@ module.exports = (on, config) => {
     },
   });
 
-  // require("@cypress/code-coverage/task")(on, config);
+  require("@cypress/code-coverage/task")(on, config);
   // IMPORTANT to return the config object
   // with the any changed environment variables
   return config;
