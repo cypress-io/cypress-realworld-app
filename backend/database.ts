b26cfa2a946a9cc6a149ea514efc93fc7a52d696
Kevin Old <kevin@kevinold.com>
2020-05-06 15:45:19 -0500

    initialize user balance to 0 if not provided

--- backend/database.ts
+++ backend/database.ts
@@ -207,7 +207,7 @@ export const createUser = (userDetails: Partial<User>): User => {
     password,
     email: userDetails.email!,
     phoneNumber: userDetails.phoneNumber!,
-    balance: userDetails.balance!,
+    balance: userDetails.balance! || 0,
     avatar: userDetails.avatar!,
     defaultPrivacyLevel: userDetails.defaultPrivacyLevel!,
     createdAt: new Date(),
