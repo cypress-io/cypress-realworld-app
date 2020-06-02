41b0fdd495416b2889bf6250c59db5cf3969fc8a
Kevin Old <kevin@cypress.io>
2020-06-02 15:25:06 -0500

    organize auth commands in separate file for readability

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -12,61 +12,6 @@ Cypress.Commands.add("getBySelLike", (selector, ...args) => {
   return cy.get(`[data-test*=${selector}]`, ...args);
 });
 
-Cypress.Commands.add("login", (username, password, rememberUser = false) => {
-  const signinPath = "/signin";
-  const log = Cypress.log({
-    name: "login",
-    displayName: "LOGIN",
-    message: [`🔐 Authenticating | ${username}`],
-    // @ts-ignore
-    autoEnd: false,
-  });
-
-  cy.server();
-  cy.route("POST", "/login").as("loginUser");
-  cy.route("GET", "checkAuth").as("getUserProfile");
-
-  cy.location("pathname", { log: false }).then((currentPath) => {
-    if (currentPath !== signinPath) {
-      cy.visit(signinPath);
-    }
-  });
-
-  log.snapshot("before");
-
-  cy.getBySel("signin-username").type(username);
-  cy.getBySel("signin-password").type(password);
-
-  if (rememberUser) {
-    cy.getBySel("signin-remember-me").find("input").check();
-  }
-
-  cy.getBySel("signin-submit").click();
-  cy.wait("@loginUser").then((loginUser) => {
-    log.set({
-      consoleProps() {
-        return {
-          username,
-          password,
-          rememberUser,
-          // @ts-ignore
-          userId: loginUser.response.body.user.id,
-        };
-      },
-    });
-
-    log.snapshot("after");
-    log.end();
-  });
-});
-
-Cypress.Commands.add("loginByApi", (username, password = Cypress.env("defaultPassword")) => {
-  return cy.request("POST", `${Cypress.env("apiUrl")}/login`, {
-    username,
-    password,
-  });
-});
-
 Cypress.Commands.add("reactComponent", { prevSubject: "element" }, ($el) => {
   if ($el.length !== 1) {
     throw new Error(`cy.component() requires element of length 1 but got ${$el.length}`);
@@ -98,69 +43,6 @@ Cypress.Commands.add("setTransactionAmountRange", (min, max) => {
     .reactComponent()
     .its("memoizedProps")
     .invoke("onChange", null, [min / 10, max / 10]);
-});
-
-Cypress.Commands.add("loginByXstate", (username, password = Cypress.env("defaultPassword")) => {
-  const log = Cypress.log({
-    name: "loginbyxstate",
-    displayName: "LOGIN BY XSTATE",
-    message: [`🔐 Authenticating | ${username}`],
-    // @ts-ignore
-    autoEnd: false,
-  });
-
-  cy.server();
-  cy.route("POST", "/login").as("loginUser");
-  cy.route("GET", "/checkAuth").as("getUserProfile");
-  cy.visit("/signin", { log: false }).then(() => {
-    log.snapshot("before");
-  });
-
-  cy.window({ log: false }).then((win) => win.authService.send("LOGIN", { username, password }));
-
-  return cy.wait("@loginUser").then((loginUser) => {
-    log.set({
-      consoleProps() {
-        return {
-          username,
-          password,
-          // @ts-ignore
-          userId: loginUser.response.body.user.id,
-        };
-      },
-    });
-
-    log.snapshot("after");
-    log.end();
-  });
-});
-
-Cypress.Commands.add("logoutByXstate", () => {
-  cy.server();
-  cy.route("POST", "/logout").as("logoutUser");
-
-  const log = Cypress.log({
-    name: "logoutByXstate",
-    displayName: "LOGOUT BY XSTATE",
-    message: [`🔒 Logging out current user`],
-    // @ts-ignore
-    autoEnd: false,
-  });
-
-  cy.window({ log: false }).then((win) => {
-    log.snapshot("before");
-    win.authService.send("LOGOUT");
-  });
-
-  return cy.wait("@logoutUser").then(() => {
-    log.snapshot("after");
-    log.end();
-  });
-});
-
-Cypress.Commands.add("switchUser", (username) => {
-  cy.logoutByXstate();
-  return cy.loginByXstate(username);
 });
 
 Cypress.Commands.add("createTransaction", (payload) => {
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,3 +1,5 @@
 // @ts-check
 import "@cypress/code-coverage/support";
 import "./commands";
+import "./auth-commands";
+//import "./example-commands/data-initialization-guide-commands";
