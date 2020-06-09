1ddd98342fe73d6e092a541d5d670c1aab910a51
Kevin Old <kevin@cypress.io>
2020-06-09 16:14:19 -0500

    cleanup

--- src/machines/publicTransactionsMachine.ts
+++ src/machines/publicTransactionsMachine.ts
@@ -2,8 +2,9 @@ import { dataMachine } from "./dataMachine";
 import axios from "axios";
 import { isEmpty, omit } from "lodash/fp";
 
-// Implement as separate method so that cy.stub can be used from test to mock data
-const fetchData = async (ctx: any, event: any) => {
+let initialContext = undefined;
+
+const fetchPublicTransactionsData = async (ctx: any, event: any) => {
   const payload = omit("type", event);
   const resp = await axios
     .create({ withCredentials: true })
@@ -12,8 +13,6 @@ const fetchData = async (ctx: any, event: any) => {
     });
   return resp.data;
 };
-
-let initialContext = undefined;
 
 // @ts-ignore
 if (window.Cypress) {
@@ -29,30 +28,8 @@ export const publicTransactionsMachine = dataMachine(
   initialContext
 ).withConfig({
   services: {
-    fetchData,
+    fetchData: fetchPublicTransactionsData,
   },
 });
 
-// @ts-ignore
-/*
-if (window.Cypress) {
-  // @ts-ignore
-  window.fetchPublicTransactions = fetchData;
-}
-*/
-
-/* in test
-// @ts-ignore
-const createFakeTransactions = (transactionCount) => {
-  //
-};
-const fakeTransactionData = () => createFakeTransactions(10);
-
-Cypress.on("win:before:load", (win) => {
-  cy.stub(win, "fetchPublicTransactions", fakeTransactionData);
-});
-cy.visit("/")
-
-cy.window().its("fetchPublicTransactions").should("have.callCount", 1);
-cy.window().its("fetchPublicTransactions").should("have.length", 10);
-*/
+export default { fetchPublicTransactionsData };
