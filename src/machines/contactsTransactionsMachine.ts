e1f44c7f06deed3a5c9543470ba974c491500435
Kevin Old <kevin@cypress.io>
2020-07-13 14:42:01 -0500

    update machines to use httpClient instead of axios directly

--- src/machines/contactsTransactionsMachine.ts
+++ src/machines/contactsTransactionsMachine.ts
@@ -1,16 +1,14 @@
 import { dataMachine } from "./dataMachine";
-import axios from "axios";
+import { httpClient } from "../utils/asyncUtils";
 import { isEmpty, omit } from "lodash/fp";
 
 export const contactsTransactionsMachine = dataMachine("contactsTransactions").withConfig({
   services: {
     fetchData: async (ctx, event: any) => {
       const payload = omit("type", event);
-      const resp = await axios
-        .create({ withCredentials: true })
-        .get(`http://localhost:3001/transactions/contacts`, {
-          params: !isEmpty(payload) ? payload : undefined,
-        });
+      const resp = await httpClient.get(`http://localhost:3001/transactions/contacts`, {
+        params: !isEmpty(payload) ? payload : undefined,
+      });
       return resp.data;
     },
   },
