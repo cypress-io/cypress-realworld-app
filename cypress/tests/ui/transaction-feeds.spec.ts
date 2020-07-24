438a6e44fba6ab87ed90528e6dec3deb0dc053f3
Kevin Old <kevin@cypress.io>
2020-07-24 15:59:36 -0500

    explicit visit to public tab

--- cypress/tests/ui/transaction-feeds.spec.ts
+++ cypress/tests/ui/transaction-feeds.spec.ts
@@ -377,6 +377,8 @@ describe("Transaction Feed", function () {
         ctx.contactIds = contacts.map((contact) => contact.contactUserId);
       });
 
+      cy.visit("/public");
+
       cy.wait("@publicTransactions")
         .its("response.body.results")
         .then((transactions: TransactionResponseItem[]) => {
