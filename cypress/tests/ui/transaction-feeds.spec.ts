95ad6c49ba92f1ea5a0eceb5cc3a18f8c3d35ba5
Kevin Old <kevin@cypress.io>
2020-07-24 14:33:10 -0500

    skip next to last test

--- cypress/tests/ui/transaction-feeds.spec.ts
+++ cypress/tests/ui/transaction-feeds.spec.ts
@@ -354,7 +354,7 @@ describe("Transaction Feed", function () {
       });
     });
 
-    it("friends feed only shows contact transactions", function () {
+    it.skip("friends feed only shows contact transactions", function () {
       cy.database("filter", "contacts", { userId: ctx.user!.id }).then((contacts: Contact[]) => {
         const contactIds = contacts.map((contact) => contact.contactUserId);
         cy.visit("/contacts");
@@ -372,7 +372,7 @@ describe("Transaction Feed", function () {
       });
     });
 
-    it.skip("first five items belong to contacts in public feed", function () {
+    it("first five items belong to contacts in public feed", function () {
       cy.database("filter", "contacts", { userId: ctx.user!.id }).then((contacts: Contact[]) => {
         const contactIds = contacts.map((contact) => contact.contactUserId);
 
