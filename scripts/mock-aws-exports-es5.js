3ebac688ac43a4a35e960c113045e16e0bceb5f6
Kevin Old <kevin@cypress.io>
2020-11-04 12:34:51 -0600

    add es5 mock script and adjust scripts

--- scripts/mock-aws-exports-es5.js
+++ scripts/mock-aws-exports-es5.js
@@ -1,8 +1,8 @@
-// mock aws-exports.js
+// mock aws-exports-es5.js
 
 const awsmobile = {
   aws_cognito_region: "",
   aws_user_pools_id: "",
 };
 
-export default awsmobile;
+exports.default = awsmobile;
