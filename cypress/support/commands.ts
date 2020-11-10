a4fcea9a388f53457f805ba94febb6ab2f2b6285
Kevin Old <kevin@cypress.io>
2020-11-10 11:32:21 -0600

    update to use React v17 fiber instance with note for React v16

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -71,7 +71,8 @@ Cypress.Commands.add("reactComponent", { prevSubject: "element" }, ($el) => {
   if ($el.length !== 1) {
     throw new Error(`cy.component() requires element of length 1 but got ${$el.length}`);
   }
-  const key = Object.keys($el.get(0)).find((key) => key.startsWith("__reactInternalInstance$"));
+  // Query for key starting with __reactInternalInstance$ for React v16.x
+  const key = Object.keys($el.get(0)).find((key) => key.startsWith("__reactFiber$"));
 
   // @ts-ignore
   const domFiber = $el.prop(key);
