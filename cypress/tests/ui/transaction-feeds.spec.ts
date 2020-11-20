9fa7b563fce8d208f515e3defa2ad0da39c26bc8
Kevin Old <kevin@cypress.io>
2020-11-20 10:59:47 -0600

    reproduction and notes around cy.http issue with public transactions stub

--- cypress/tests/ui/transaction-feeds.spec.ts
+++ cypress/tests/ui/transaction-feeds.spec.ts
@@ -47,9 +47,9 @@ describe("Transaction Feed", function () {
     cy.task("db:seed");
 
     cy.http({ method: "GET", url: "http://localhost:3001/notifications" }).as("notifications");
-    cy.http("/transactions*").as(feedViews.personal.routeAlias);
-    cy.http("/transactions/public*").as(feedViews.public.routeAlias);
-    cy.http("/transactions/contacts*").as(feedViews.contacts.routeAlias);
+    cy.http("GET", "**/transactions*").as(feedViews.personal.routeAlias);
+    cy.http("GET", "http://localhost:3001/transactions/public*").as(feedViews.public.routeAlias); //publicTransactions
+    cy.http("GET", "**/transactions/contacts*").as(feedViews.contacts.routeAlias);
 
     cy.database("filter", "users").then((users: User[]) => {
       ctx.user = users[0];
@@ -85,11 +85,10 @@ describe("Transaction Feed", function () {
   });
 
   describe("renders and paginates all transaction feeds", function () {
-    it.only("renders transactions item variations in feed", function () {
-      cy.http("GET", "/", (req) => {
-        console.log("page loaded");
-      });
-      cy.http("**/transactions/public*", {
+    it("renders transactions item variations in feed", function () {
+      // Cannot properly wait for request against endpoint with multiple aliases
+      // Bug: Stub response for Public Transactions
+      cy.http("GET", "http://localhost:3001/transactions/public*", {
         headers: {
           "access-control-allow-origin": window.location.origin,
           "Access-Control-Allow-Credentials": "true",
@@ -97,10 +96,10 @@ describe("Transaction Feed", function () {
         fixture: "public-transactions.json",
       }).as("mockedPublicTransactions");
 
+      // Visit page again to trigger call to /transactions/public
       cy.visit("/");
 
-      cy.wait("@notifications");
-      cy.wait("@notifications");
+      //cy.wait("@notifications");
       cy.wait("@mockedPublicTransactions")
         .its("response.body.results")
         .then(JSON.parse)
