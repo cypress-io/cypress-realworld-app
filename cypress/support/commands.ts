db748e6cc5e534d1788d632511095f39e58559d4
Kevin Old <kevin@cypress.io>
2020-12-15 14:18:36 -0600

    remove okta command

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -5,8 +5,6 @@ import { pick } from "lodash/fp";
 import { format as formatDate } from "date-fns";
 import "@percy/cypress";
 import { isMobile } from "./utils";
-// @ts-ignore
-import OktaAuth from "@okta/okta-auth-js";
 
 // custom command to make taking snapshots with full name
 // formed from the test title + suffix easier
@@ -319,53 +317,5 @@ Cypress.Commands.add("database", (operation, entity, query, logTask = false) => 
     log.snapshot();
     log.end();
     return data;
-  });
-});
-
-// Okta
-Cypress.Commands.add("loginByOktaApi", (username: string, password: string) => {
-  cy.log(`Logging in as ${username}`);
-
-  cy.request({
-    method: "POST",
-    url: `https://${Cypress.env("okta_domain")}/api/v1/authn`,
-    body: {
-      username,
-      password,
-    },
-  }).then(({ body }) => {
-    const user = body._embedded.user;
-    const config = {
-      issuer: `https://${Cypress.env("okta_domain")}/oauth2/default`,
-      clientId: Cypress.env("okta_client_id"),
-      redirectUri: "http://localhost:3000/implicit/callback",
-      scope: ["openid", "email", "profile"],
-    };
-
-    const authClient = new OktaAuth(config);
-
-    return authClient.token
-      .getWithoutPrompt({
-        sessionToken: body.sessionToken,
-        responseType: "id_token",
-      })
-      .then((res: any) => {
-        const tokens = res.tokens;
-
-        const userItem = {
-          token: tokens.accessToken.value,
-          user: {
-            sub: user.id,
-            email: user.profile.login,
-            given_name: user.profile.firstName,
-            family_name: user.profile.lastName,
-            preferred_username: user.profile.login,
-          },
-        };
-
-        window.localStorage.setItem("oktaCypress", JSON.stringify(userItem));
-
-        cy.visit("/");
-      });
   });
 });
