28a8a9c16a59915d3c7e1da0991101e09ebfdf28
Kevin Old <kevin@cypress.io>
2021-01-22 16:13:21 -0600

    install and configure snowpack

---
+++ snowpack.config.js
@@ -0,0 +1,32 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+
+module.exports = {
+  mount: {
+    public: { url: "/", static: true },
+    src: { url: "/dist" },
+  },
+  plugins: [
+    "@snowpack/plugin-react-refresh",
+    "@snowpack/plugin-dotenv",
+    "@snowpack/plugin-typescript",
+  ],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    // "bundle": true,
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    port: 3000,
+    src: "src",
+    bundle: false,
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
