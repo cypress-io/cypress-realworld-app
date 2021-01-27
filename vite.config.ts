4dcc5c4113eb8472cd4e18a6a6492a04158e879a
Kevin Old <kevin@cypress.io>
2021-01-27 13:34:18 -0600

    updates per https://github.com/vitejs/vite/issues/1652\#issuecomment-765875146

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+  optimizeDeps: {
+    include: ["lodash/fp"],
+  },
+  plugins: [reactRefresh(), svgr()],
 });
