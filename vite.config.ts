bf517b5c52caa7c3172e052e4a0b20a20d04bb0b
Kevin Old <kevin@cypress.io>
2021-02-19 10:05:46 -0600

    configure vite proxy and cors for yarn dev:vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import { defineConfig } from "vite";
-import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   optimizeDeps: {
+    entries: ["index.html"],
     include: ["lodash/fp"],
+    exclude: ["./coverage", "./build"],
   },
-  plugins: [reactRefresh(), svgr()],
+  server: {
+    cors: { origin: "https://localhost:3000", credentials: true },
+    proxy: {
+      "/login": "http://localhost:3001/login",
+      "/callback": "http://localhost:3001/callback",
+      "/logout": "http://localhost:3001/logout",
+      "/checkAuth": "http://localhost:3001/checkAuth",
+    },
+  },
+  plugins: [reactRefresh()],
 });
