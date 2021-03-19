8d9e35d923213fe723bcce4709a0716b6a715d13
Jessica Sachs <jess@jessicasachs.io>
2021-03-19 01:34:22 -0400

    wip: alert-bar.tsx is failing because of react-dom issue

--- src/components/__tests__/AlertBar.test.tsx
+++ src/components/__tests__/AlertBar.test.tsx
@@ -4,13 +4,13 @@ import React from "react";
 import ReactDom from "react-dom";
 import { mount } from "@cypress/react";
 import { snackbarMachine } from "../../machines/snackbarMachine";
-import { useMachine } from '@xstate/react'
+import { useMachine } from "@xstate/react";
 
 import AlertBar from "../AlertBar";
 
 describe("smoke", () => {
   it("works", () => {
-    const [, , snackbarService] = useMachine(snackbarMachine);    
+    const [, , snackbarService] = useMachine(snackbarMachine);
     mount(<AlertBar snackbarService={snackbarService} />, { ReactDom });
     // This works fine! :-(
     // mount(<button>Hello world!</button>);
