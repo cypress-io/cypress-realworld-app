30851b5da832d14ad5c2728c15987739546afd8d
Kevin Old <kevin@cypress.io>
2021-03-25 17:22:17 -0500

    add delay middleware for all mobile viewport tests

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,12 +1,26 @@
-// @ts-check
+/* eslint-disable */
+// @ts-nocheck
 import "@cypress/code-coverage/support";
 import "./commands";
+import { isMobile } from "./utils";
 
 beforeEach(() => {
+  // cy.intercept middleware to remove 'if-none-match' headers from all requests
+  // to prevent the server from returning cached responses of API requests
   cy.intercept(
-    // requires Cypress 7.x and higher for middleware flag
-    // and fix in https://github.com/cypress-io/cypress/pull/14543
     { url: "http://localhost:3001", middleware: true },
     (req) => delete req.headers["if-none-match"]
   );
+
+  // If under mobile viewport, apply cy.intercept middleware to delay the response
+  // for all routes except /login
+  if (isMobile()) {
+    cy.intercept({ url: "http://localhost:3001", middleware: true }, (req) => {
+      req.on("response", (res) => {
+        if (req.url !== "http://localhost:3001/login") {
+          res.delay(2500);
+        }
+      });
+    });
+  }
 });
