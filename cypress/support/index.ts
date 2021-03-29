aa4fb9b605cb81102c18cc073325b8dfba6390b9
Kevin Old <kevin@cypress.io>
2021-03-29 11:10:16 -0500

    update mobile cy.intercept to throttle all routes

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -11,14 +11,12 @@ beforeEach(() => {
     (req) => delete req.headers["if-none-match"]
   );
 
-  // If under mobile viewport, apply cy.intercept middleware to delay the response
-  // for all routes except /login
+  // Throttle API responses for mobile testing to simulate real world condition
   if (isMobile()) {
     cy.intercept({ url: "http://localhost:3001", middleware: true }, (req) => {
       req.on("response", (res) => {
-        if (req.url !== "http://localhost:3001/login") {
-          res.delay(2500);
-        }
+        // Throttle the response to 1 Mbps to simulate a mobile 3G connection
+        res.throttle(1000
       });
     });
   }
