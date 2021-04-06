05e6a92f9ca5e02b0e3fac7f5d3b67a239951963
Kevin Old <kevin@cypress.io>
2021-04-06 10:43:14 -0500

    fix patch matching

--- cypress/tests/ui/user-settings.spec.ts
+++ cypress/tests/ui/user-settings.spec.ts
@@ -5,8 +5,8 @@ describe("User Settings", function () {
   beforeEach(function () {
     cy.task("db:seed");
 
-    cy.intercept("PATCH", "/users").as("updateUser");
-    cy.intercept("GET", "/notifications").as("getNotifications");
+    cy.intercept("PATCH", "/users/*").as("updateUser");
+    cy.intercept("GET", "/notifications*").as("getNotifications");
 
     cy.database("find", "users").then((user: User) => {
       cy.loginByXstate(user.username);
