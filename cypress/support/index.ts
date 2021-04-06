5f6399a7ff00715a1da739fb1ffdacf7a3165d76
Kevin Old <kevin@cypress.io>
2021-04-06 08:36:04 -0500

    update matching for global intercepts

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -7,13 +7,13 @@ beforeEach(() => {
   // cy.intercept middleware to remove 'if-none-match' headers from all requests
   // to prevent the server from returning cached responses of API requests
   cy.intercept(
-    { url: "http://localhost:3001", middleware: true },
+    { url: "http://localhost:3001*", middleware: true },
     (req) => delete req.headers["if-none-match"]
   );
 
   // Throttle API responses for mobile testing to simulate real world condition
   if (isMobile()) {
-    cy.intercept({ url: "http://localhost:3001", middleware: true }, (req) => {
+    cy.intercept({ url: "http://localhost:3001*", middleware: true }, (req) => {
       req.on("response", (res) => {
         // Throttle the response to 1 Mbps to simulate a mobile 3G connection
         res.setThrottle(1000);
