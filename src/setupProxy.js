286727effd30b5f6af13b52814ff6b78431434e9
Kevin Old <kevin@cypress.io>
2021-05-04 13:47:04 -0500

    fix setupProxy to use process.env.BACKEND_PORT directly

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,10 @@
 const createProxyMiddleware = require("http-proxy-middleware");
-
-const { backendPort } = require("./utils/portUtils");
+require("dotenv").config();
 
 module.exports = function (app) {
   app.use(
     createProxyMiddleware(["/login", "/callback", "/logout", "/checkAuth", "graphql"], {
-      target: `http://localhost:${backendPort}`,
+      target: `http://localhost:${process.env.BACKEND_PORT}`,
       changeOrigin: true,
       logLevel: "debug",
     })
