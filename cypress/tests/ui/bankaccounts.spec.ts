a2d418f6126328ba89c49700271aee240e1541da
Kevin Old <kevin@cypress.io>
2021-05-18 16:11:15 -0500

    comment out graphql status code check

--- cypress/tests/ui/bankaccounts.spec.ts
+++ cypress/tests/ui/bankaccounts.spec.ts
@@ -30,7 +30,7 @@ describe("Bank Accounts", function () {
       }
 
       // Check the return of every graphql call to be 200 to prevent flake
-      req.reply((res) => expect(res.statusCode).to.equal(200));
+      //req.reply((res) => expect(res.statusCode).to.equal(200));
     });
 
     cy.database("find", "users").then((user: User) => {
