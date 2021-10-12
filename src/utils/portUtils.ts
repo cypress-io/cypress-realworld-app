c1861999c7c3cb7fe50c0a6f08d56bfb642cbc90
Robert Guss <robertguss@pm.me>
2021-10-12 13:13:18 -0400

    Issue #1114 - Updated PORT .env variables to change default CRA ports. (#1115)
    * added a PORT .env variable to change react's front-end default port. Updated portUtils.ts to use this new variable
    
    * updated backend port to be the default of 3001

--- src/utils/portUtils.ts
+++ src/utils/portUtils.ts
@@ -1,4 +1,4 @@
 require("dotenv").config();
 
-export const frontendPort = process.env.REACT_APP_PORT;
+export const frontendPort = process.env.PORT;
 export const backendPort = process.env.REACT_APP_BACKEND_PORT;
