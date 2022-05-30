d2d9a080cec71570920ceacdf35149f2181ed817
b0ratt <maciej.boratyn94@gmail.com>
2022-05-30 19:28:07 +0200

    remove beforeEach

--- cypress/tests/ui/auth.spec.ts
+++ cypress/tests/ui/auth.spec.ts
@@ -4,19 +4,6 @@ import { isMobile } from "../../support/utils";
 const apiGraphQL = `${Cypress.env("apiUrl")}/graphql`;
 
 describe("User Sign-up and Login", function () {
-  beforeEach(function () {
-    cy.task("db:seed");
-
-    cy.intercept("POST", "/users").as("signup");
-    cy.intercept("POST", apiGraphQL, (req) => {
-      const { body } = req;
-
-      if (body.hasOwnProperty("operationName") && body.operationName === "CreateBankAccount") {
-        req.alias = "gqlCreateBankAccountMutation";
-      }
-    });
-  });
-
   it("should redirect unauthenticated user to signin page", function () {
     cy.visit("/personal");
     cy.location("pathname").should("equal", "/signin");
