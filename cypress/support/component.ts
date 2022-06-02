36d1abc34b3cab54aaa544700b620c9aba6e2180
Robert Guss <robertguss@pm.me>
2022-06-02 09:20:47 -0400

    removed cusotm mound command from component.ts file

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -1,13 +1,2 @@
 import "@cypress/code-coverage/support";
 import "./commands";
-
-import { mount } from "cypress/react";
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      mount: typeof mount;
-    }
-  }
-}
-
-Cypress.Commands.add("mount", mount);
