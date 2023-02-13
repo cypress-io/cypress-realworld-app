02c76ac14716b2d701b6cd24a398d68d2879ff82
Adam Stone-Lord <adams@cypress.io>
2023-02-12 23:50:09 -0500

    chore: use React 18 render API (#1308)

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -20,7 +20,7 @@ import "./commands";
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-import { mount } from "cypress/react";
+import { mount } from "cypress/react18";
 
 Cypress.Commands.add("mount", mount);
 
