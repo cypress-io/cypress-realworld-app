2ef49aedcdf7a4d1d93aacca95ac8fa2d69a31d7
Mohsen Azimi <mohsen.azimi@airbnb.com>
2023-06-23 14:40:59 +0200

    Add form validation

--- playwright/__tests__/bankaccounts.test.ts
+++ playwright/__tests__/bankaccounts.test.ts
@@ -41,5 +41,81 @@ test.describe("Bank Accounts", () => {
       // Expect two bank accounts in the list
       expect(accounts).toBe(2);
     });
+
+    test.only("Bank Account form error validations", async ({ page }) => {
+      await page.getByTestId("sidenav-bankaccounts").click();
+
+      // Check for navigation
+      expect(page.url()).toMatch("/bankaccounts");
+
+      await page.getByTestId("bankaccount-new").click();
+      // Fill the input
+      await page.getByPlaceholder("Bank Name").fill("The");
+
+      await page.pause();
+
+      // Clear the input and trigger blur
+      await page.getByPlaceholder("Bank Name").fill("");
+      await page.getByPlaceholder("Bank Name").press("Tab");
+
+      // Check the validation message
+      expect(await page.locator("#bankaccount-bankName-input-helper-text").textContent()).toContain(
+        "Enter a bank name"
+      );
+
+      // Fill the input with less than 9 digits
+      await page.getByPlaceholder("Routing Number").fill("12345678");
+      await page.getByPlaceholder("Routing Number").press("Tab");
+
+      // Check the validation message
+      expect(
+        await page.locator("#bankaccount-routingNumber-input-helper-text").textContent()
+      ).toContain("Must contain a valid routing number");
+
+      // Fill the input with exactly 9 digits
+      await page.getByPlaceholder("Routing Number").fill("123456789");
+      await page.getByPlaceholder("Routing Number").press("Tab");
+
+      // Check that the validation message does not exist
+      expect(await page.locator("#bankaccount-routingNumber-input-helper-text").isVisible()).toBe(
+        false
+      );
+
+      // Fill the input with less than 9 digits
+      await page.getByPlaceholder("Account Number").fill("12345678");
+      await page.getByPlaceholder("Account Number").press("Tab");
+
+      // Check the validation message
+      expect(
+        await page.locator("#bankaccount-accountNumber-input-helper-text").textContent()
+      ).toContain("Must contain at least 9 digits");
+
+      // Fill the input with exactly 9 digits
+      await page.getByPlaceholder("Account Number").fill("123456789");
+      await page.getByPlaceholder("Account Number").press("Tab");
+
+      // Check that the validation message does not exist
+      expect(await page.locator("#bankaccount-accountNumber-input-helper-text").isVisible()).toBe(
+        false
+      );
+
+      // Fill the input with exactly 12 digits
+      await page.getByPlaceholder("Account Number").fill("123456789111");
+      await page.getByPlaceholder("Account Number").press("Tab");
+
+      // Check that the validation message does not exist
+      expect(await page.locator("#bankaccount-accountNumber-input-helper-text").isVisible()).toBe(
+        false
+      );
+
+      // Fill the input with more than 12 digits
+      await page.getByPlaceholder("Account Number").fill("1234567891111");
+      await page.getByPlaceholder("Account Number").press("Tab");
+
+      // Check the validation message
+      expect(
+        await page.locator("#bankaccount-accountNumber-input-helper-text").textContent()
+      ).toContain("Must contain no more than 12 digits");
+    });
   });
 });
