1ed0add5b988f8420ddf5509ae0faf929ee7e51e
Ely Lucas <ely@meta-tek.net>
2023-07-11 10:18:30 -0600

    adding vite eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "VITE");
@@ -15,6 +16,6 @@ export default defineConfig(({ command, mode }) => {
       outDir: "build",
     },
 
-    plugins: [react()],
+    plugins: [react(), eslint()],
   };
 });
