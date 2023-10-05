744b3af59562afc3311ce4cba24c3d679087a9c1
Natalie <natalielunbeck@yahoo.com>
2023-10-05 13:06:41 -0700

    remove linter

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
 import istanbul from "vite-plugin-istanbul";
 
 export default defineConfig(({ command, mode }) => {
@@ -20,7 +19,6 @@ export default defineConfig(({ command, mode }) => {
     },
     plugins: [
       react(),
-      eslint(),
       istanbul({
         cypress: true,
         requireEnv: true,
