95e8965485ab22cc6f168d5dec80553de170b0e3
Natalie <natalielunbeck@yahoo.com>
2023-10-09 11:36:18 -0700

    hardcode baseUrl

--- cypress.config.ts
+++ cypress.config.ts
@@ -65,7 +65,8 @@ module.exports = defineConfig({
     },
   },
   e2e: {
-    baseUrl: `https://frontend-${process.env.SHIPYARD_DOMAIN_FRONTEND}`,
+    //baseUrl: `https://${process.env.SHIPYARD_DOMAIN_FRONTEND}`,
+    baseUrl: "https://frontend-cypress-realworld-app.dev.nbeck415.shipyard.host",
     specPattern: "cypress/tests/**/*.spec.{js,jsx,ts,tsx}",
     supportFile: "cypress/support/e2e.ts",
     viewportHeight: 1000,
