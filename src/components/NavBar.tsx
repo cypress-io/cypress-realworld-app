ea9a4a6f463e67abdca22871ddaf30ee5279c1e0
Tim Heilman <tdh@stanfordalumni.org>
2024-03-05 17:36:07 -0800

    fix: remove zIndex from AppBar to fix drawer issue
    appBar was still over the date picker in mobile screen size, failing one of the cypress:run:mobile tests.  See also
    https://mui.com/material-ui/migration/v5-component-changes/\#fix-z-index-issues

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -39,7 +39,6 @@ const useStyles = makeStyles((theme) => ({
     paddingRight: 24, // keep right padding when drawer closed
   },
   appBar: {
-    zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
