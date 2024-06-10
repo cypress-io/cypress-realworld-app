cd6c00bd6cc617c999add57ab0e358b95b2e3f95
Tim Heilman <tdh@stanfordalumni.org>
2024-06-10 07:50:33 -0700

    fix: get body font size back down to 14, establish knobs for
    modifying input styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,50 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Router } from "react-router-dom";
-import { createTheme, StyledEngineProvider, adaptV4Theme } from "@mui/material";
+import { createTheme, StyledEngineProvider } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import App from "./containers/App";
 import { history } from "./utils/historyUtils";
 
-const theme = createTheme(
-  adaptV4Theme({
-    palette: {
-      secondary: {
-        main: "#fff",
+const theme = createTheme({
+  palette: {
+    secondary: {
+      main: "#fff",
+    },
+  },
+  typography: {
+    // htmlFontSize: 18.285714285714286,
+    fontSize: 14 * 0.875,
+    body1: {
+      lineHeight: 1.43,
+      letterSpacing: "0.01071em",
+    },
+  },
+  components: {
+    MuiOutlinedInput: {
+      styleOverrides: {
+        input: {
+          padding: "6px 0 7px",
+        },
       },
     },
-  })
-);
+    MuiInputBase: {
+      styleOverrides: {
+        input: {
+          padding: "6px 0 7px",
+        },
+      },
+    },
+    // MuiInput: {
+    //   defaultProps: {
+    //     inputProps: {
+    //       backgroundColor: "green",
+    //       height: "300px",
+    //     },
+    //   },
+    // },
+  },
+});
 
 const root = createRoot(document.getElementById("root")!);
 
