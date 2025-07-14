8fee5a8887a6ae43e5b64284a1b7efe9fd5b2c76
gabrielareisvf <gabrielareisvf@gmail.com>
2025-07-14 00:06:06 -0300

    First successful login

--- cypress/tests/e2e/login.spec.js
+++ cypress/tests/e2e/login.spec.js
@@ -1,6 +1,9 @@
-describe("Login com sucess", () => {
-  it("Deve fazer login com um usuário válido", () => {
-    // Implemente os passos do caso de teste aqui
-    // teste de pull
+describe("Successful Login", () => {
+  it("Should log in and show New Transaction page", () => {
+    cy.visit("http://localhost:3000/signin");
+    cy.get("[name='username']").type("Heath93");
+    cy.get("[name='password']").type("s3cret");
+    cy.get("button[type='submit']").click();
+    cy.contains("Public").should("be.visible");
   });
 });
