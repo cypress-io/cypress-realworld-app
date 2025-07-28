c2cd896e10e1fde5d86a3ba89ca3a342d7eb982e
Abhishrut Vaidya <37737991+babyitachi@users.noreply.github.com>
2025-07-28 15:35:12 +0530

    #addressing review - config files linting not required, ignoring

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-import pluginJs from "@eslint/js";
-import pluginTs from "typescript-eslint";
-import pluginTsParser from "@typescript-eslint/parser";
-import pluginCypress from "eslint-plugin-cypress";
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import pluginJs from '@eslint/js'
+import pluginTs from 'typescript-eslint'
+import pluginTsParser from '@typescript-eslint/parser'
+import pluginCypress from 'eslint-plugin-cypress'
 
 export default defineConfig([
   {
-    ignores: ["build/"],
+    ignores: ['build/'],
   },
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     languageOptions: {
       parser: pluginTsParser,
     },
@@ -21,26 +21,26 @@ export default defineConfig([
   {
     rules: {
       // TO-DO review violations of disabled rules
-      "no-empty": "off",
-      "no-prototype-builtins": "off",
-      "no-undef": "off",
-      "no-unused-vars": "off",
-      "prefer-const": "off",
-      "@typescript-eslint/ban-ts-comment": "off",
-      "@typescript-eslint/no-empty-object-type": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
-      "@typescript-eslint/no-unsafe-function-type": "off",
-      "@typescript-eslint/no-unused-expressions": "off",
-      "@typescript-eslint/no-wrapper-object-types": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-require-imports": "off",
-      "@typescript-eslint/triple-slash-reference": "off",
+      'no-empty': 'off',
+      'no-prototype-builtins': 'off',
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
+      'prefer-const': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-wrapper-object-types': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/triple-slash-reference': 'off',
     },
     languageOptions: {
       globals: {
         ...globals.node,
       },
-    },
-  },
-]);
+    }
+  }
+])
