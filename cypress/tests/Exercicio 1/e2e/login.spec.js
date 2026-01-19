4cbc20d7006aa9fa44be4ed236d7e8f40f0a7e64
Silas Rangel <silas.rangel@devexsolucoes.com.br>
2026-01-18 22:29:06 -0300

    Adding usar data jason

--- cypress/tests/Exercicio 1/e2e/login.spec.js
+++ cypress/tests/Exercicio 1/e2e/login.spec.js
@@ -1,3 +1,5 @@
+import userData from '../fixtures/userdata.json'
+
 describe('RealWorld App - Tests', () => {
 
 const selectorsList = {
@@ -11,48 +13,48 @@ const selectorsList = {
   signinError: "[data-test=\"signin-error\"]"
 }
 
-
   it('Login - Sucess', () => {
     cy.visit('http://localhost:3000/')
-    cy.get(selectorsList.usernameField).type('Arvilla_Hegmann')
-    cy.get(selectorsList.passwordField).type('s3cret');
+    cy.get(selectorsList.usernameField).type(userData.userSucces.username)
+    cy.get(selectorsList.passwordField).type(userData.userSucces.password)
     cy.get(selectorsList.loginButton).click({ force: true })
     //Automação do Caso de Teste: Login com sucesso.
   })
+
   it('Login - Failed', () => {
     cy.visit('http://localhost:3000/')
-    cy.get(selectorsList.usernameField).type('TestUser')
-    cy.get(selectorsList.passwordField).type('Test');
+    cy.get(selectorsList.usernameField).type(userData.userFailed.username)
+    cy.get(selectorsList.passwordField).type(userData.userFailed.password)
     cy.get(selectorsList.loginButton).click({ force: true })
     cy.get(selectorsList.signinError)
     //Automação do Caso de Teste: Tentar fazer login com credenciais inválidas.
-  });
+  })
 
   it('Login - Valid', () => {
     cy.visit('http://localhost:3000/')
     cy.get(selectorsList.signupButton).click({ force: true })
-    cy.get(selectorsList.firstName).type('Ted')
-    cy.get(selectorsList.lastName).type('Parisian')
-    cy.get(selectorsList.usernameField).type('Heath93')
-    cy.get(selectorsList.passwordField).type('s3cret')
-    cy.get(selectorsList.confirmPassword).type('s3cret')
+    cy.get(selectorsList.firstName).type(userData.userValid.firstName)
+    cy.get(selectorsList.lastName).type(userData.userValid.lastName)
+    cy.get(selectorsList.usernameField).type(userData.userValid.username)
+    cy.get(selectorsList.passwordField).type(userData.userValid.password)
+    cy.get(selectorsList.confirmPassword).type(userData.userValid.confirmPassword)
     cy.get(selectorsList.loginButton).click({ force: true })
-    cy.get(selectorsList.usernameField).type('Heath93')
-    cy.get(selectorsList.passwordField).type('s3cret')
+    cy.get(selectorsList.usernameField).type(userData.userValid.username)
+    cy.get(selectorsList.passwordField).type(userData.userValid.password)
     cy.get(selectorsList.loginButton).click({ force: true })
     // Automação do Caso de Teste: Registro de novo usuário com sucesso.
-  });
+  })
 
    it('Login - Required information', () => {
     cy.visit('http://localhost:3000/')
     cy.get(selectorsList.signupButton).click({ force: true })
-    cy.get(selectorsList.firstName).type('Darrel')
-    cy.get(selectorsList.lastName).type('Ortiz')    
+    cy.get(selectorsList.firstName).type(userData.incompleteUserdata.firstName)
+    cy.get(selectorsList.lastName).type(userData.incompleteUserdata.lastName)    
     cy.get(selectorsList.usernameField).focus().blur()
     cy.get(selectorsList.passwordField).focus().blur()
     cy.get(selectorsList.confirmPassword).focus().blur()
     cy.get(selectorsList.loginButton).click({ force: true })
     // Automação do Caso de Teste: Tentar registrar um novo usuário com informações incompletas.
-  });
+  })
 
-});
+})
