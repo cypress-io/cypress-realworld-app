ce175680241d3815153080e3cfa52fb7149083ba
silas <silasrangel1403@gmail.com>
2026-01-25 00:12:36 -0300

    Atualizacao do exercicio

--- cypress/tests/Exercicio 1/e2e/login.spec.js
+++ cypress/tests/Exercicio 1/e2e/login.spec.js
@@ -18,12 +18,13 @@ const selectorsList = {
     cy.get(selectorsList.usernameField).type(userData.userSucess.username)
     cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
-    cy.location('pathname', { timeout: 10000 }).should('include', '/')
-    cy.get('nav').contains(userData.userSucess.username).should('be.visible')
-    //Automação do Caso de Teste: Login com sucesso.
+    cy.get('[data-test="sidenav-username"]', { timeout: 10000 }).should('be.visible').and('contain', userData.userSucess.username)
+    cy.get('[data-test="sidenav-signout"]').should('be.visible')
+    cy.pause()
+   //Automação do Caso de Teste: Login com sucesso.
   })
 
-  it.skip('Login - Failed', () => {
+  it('Login - Failed', () => {
     cy.visit('http://localhost:3000/')
     cy.get(selectorsList.usernameField).type(userData.userFailed.username)
     cy.get(selectorsList.passwordField).type(userData.userFailed.password)
@@ -32,7 +33,7 @@ const selectorsList = {
     //Automação do Caso de Teste: Tentar fazer login com credenciais inválidas.
   })
 
-  it.skip('Login - Valid', () => {
+  it('Login - Valid', () => {
     cy.visit('http://localhost:3000/')
     cy.get(selectorsList.signupButton).click({ force: true })
     cy.get(selectorsList.firstName).type(userData.userValid.firstName)
@@ -44,10 +45,11 @@ const selectorsList = {
     cy.get(selectorsList.usernameField).type(userData.userValid.username)
     cy.get(selectorsList.passwordField).type(userData.userValid.password)
     cy.get(selectorsList.loginButton).click({ force: true })
+    cy.pause()
     // Automação do Caso de Teste: Registro de novo usuário com sucesso.
   })
 
-   it.skip('Login - Required information', () => {
+   it('Login - Required information', () => {
     cy.visit('http://localhost:3000/')
     cy.get(selectorsList.signupButton).click({ force: true })
     cy.get(selectorsList.firstName).type(userData.incompleteUserdata.firstName)
